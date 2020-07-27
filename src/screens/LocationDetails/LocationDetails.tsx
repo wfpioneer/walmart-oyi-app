435aadb337db85fef21bbb8d00314ebe5728778e
vn50pyy <vn50pyy@homeoffice.wal-mart.com>
2020-07-27 15:59:49 -0500

    Fixed navigation; route information now properly contains params.

--- src/screens/LocationDetails/LocationDetails.tsx
+++ src/screens/LocationDetails/LocationDetails.tsx
@@ -6,21 +6,23 @@ import { strings } from '../../locales';
 import Location from '../../models/Location';
 import { useRoute } from "@react-navigation/native";
 
-export interface Props {
-    floorLocs?: [Location],
-    resLocs?: [Location]
-};
-
 //TODO: Validate functionality, attach to the rest of the project.  THIS IS NOT COMPLETE AND NOT READY FOR MERGER.
 
-function LocationDetails(props:Props) {
-    const floorList=(props.floorLocs?props.floorLocs:[]);
-    console.log(props.floorLocs?.length);
-    console.log(floorList);
-    const resList=(props.resLocs?props.resLocs:[]);
-    const displayFloorLocs = (props.floorLocs&&props.floorLocs.length>0)?createLocations(floorList):<View></View>;
-    const displayReserveLocs = (props.resLocs&&props.resLocs.length>0)?createLocations(resList):<View></View>;
-    function createLocations(locationList : [Location]) {
+export interface locationProps {
+    floorLoc?: [Location],
+    resLoc?: [Location]
+  };
+
+function LocationDetails() {
+    const route=useRoute();
+    console.log(route)
+    const locProps:locationProps = route.params?route.params:{};
+    const floorList=(locProps.floorLoc?locProps.floorLoc:[]);
+    console.log(route.params);
+    const resList=(locProps.resLoc?locProps.resLoc:[]);
+    const displayFloorLocs = (locProps.floorLoc&&locProps.floorLoc.length>0)?createLocations(floorList):<View></View>;
+    const displayReserveLocs = (locProps.resLoc&&locProps.resLoc.length>0)?createLocations(resList):<View></View>;
+    function createLocations(locationList:[Location]) {
         locationList.forEach(element => {
             return (
             <LocationDetailsCard locationName={element.name} locationType={element.type} />
@@ -29,9 +31,9 @@ function LocationDetails(props:Props) {
     };
     return(
         <SafeAreaView>
-            {props.floorLocs?<View style={styles.sectionLabel}><Text>{strings(LOCATION.FLOOR)} ({props.floorLocs.length})</Text></View>:<View></View>}
+            {locProps.floorLoc?<View style={styles.sectionLabel}><Text>{strings('LOCATION.FLOOR')} ({locProps.floorLoc.length})</Text></View>:<View></View>}
             {displayFloorLocs}
-            {props.resLocs?<View style={styles.sectionLabel}><Text>{strings(LOCATION.RESERVE)} ({props.resLocs.length})</Text></View>:<View></View>}
+            {locProps.resLoc?<View style={styles.sectionLabel}><Text>{strings('LOCATION.RESERVE')} ({locProps.resLoc.length})</Text></View>:<View></View>}
             {displayReserveLocs}
         </SafeAreaView>
     );
