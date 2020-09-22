31038f3ef3f2620f0104380d86598f6dfefd070b
t0s00og <Timothy.Strickland@walmart.com>
2020-09-22 16:55:31 -0500

    fixed grammar issues

--- src/locales/en.js
+++ src/locales/en.js
@@ -123,8 +123,8 @@ export default {
     RESERVE: 'Reserve locations',
     ADD_LOCATION_API_ERROR: 'There was an error adding the location. \nPlease try again.',
     EDIT_LOCATION_API_ERROR: 'There was an error editing the location. \nPlease try again.',
-    ADD_DUPLICATE_ERROR: 'Location and type combination \nalready exist.',
-    EDIT_DUPLICATE_ERROR: 'Location and Type combination \nalready exist.',
+    ADD_DUPLICATE_ERROR: 'Location and type combination \nalready exists.',
+    EDIT_DUPLICATE_ERROR: 'Location and type combination \nalready exists.',
     MANUAL_ENTRY_BUTTON: 'Manually key in location',
     SELECTION_INSTRUCTION: '1. Select a location type.',
     SCAN_INSTRUCTION: '2. Scan location label.'
