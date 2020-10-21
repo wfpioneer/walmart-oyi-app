0f237a70746d864f6c973472a17c2bb4ef9751b9
Nathan Edwards <nathan.edwards0@walmart.com>
2020-10-21 13:34:49 -0500

    Use the unix timestamp in state instead of the moment object

--- src/state/reducers/SessionTimeout.ts
+++ src/state/reducers/SessionTimeout.ts
@@ -3,9 +3,7 @@ const initialState = null;
 export const SessionTimeout = (state = initialState, action: any) => {
   switch (action.type) {
     case 'SESSION/ENDTIME':
-      return {
-        ...action.payload
-      };
+      return action.payload;
     case 'SESSION/CLEAR':
       return initialState;
     default:
--- src/utils/sessionTimeout.ts
+++ src/utils/sessionTimeout.ts
@@ -9,12 +9,13 @@ import { clearEndTime } from '../state/actions/SessionTimeout';
 const sessionLength = 2;
 export const sessionEnd = () => {
   const endTime = moment();
-  return endTime.add(sessionLength, 'hours');
+  endTime.add(sessionLength, 'hours');
+  return endTime.unix();
 };
 
 export function validateSession(navigation: any): (void) {
   const endTime = store.getState().SessionTimeout;
-  if (moment().isSameOrAfter(endTime)) {
+  if (moment().isSameOrAfter(moment(endTime))) {
     trackEvent('user_sign_out');
     clearEndTime();
     WMSSO.signOutUser().then(() => {
