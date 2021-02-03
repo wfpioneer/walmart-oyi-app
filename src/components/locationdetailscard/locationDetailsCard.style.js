ab1ecb0b0c26b434e8a27b21f38f43d5bc5f1268
r0m08be <ryan.morgan1@gmail.com>
2021-02-03 14:34:32 -0600

    Updating to version 1.0.6 for release

--- src/components/locationdetailscard/locationDetailsCard.style.js
+++ src/components/locationdetailscard/locationDetailsCard.style.js
@@ -1,36 +1,36 @@
-import {StyleSheet} from "react-native";
-import {COLOR} from "../../themes/Color";
+import { StyleSheet } from 'react-native';
+import { COLOR } from '../../themes/Color';
 
-const styles=StyleSheet.create ({
-    card: {
-        width: "100%",
-        height: 70,
-        backgroundColor: COLOR.WHITE,
-        paddingLeft: 10,
-        paddingRight: 10,
-        flexDirection: "row",
-        alignItems: "center",
-    },
-    location: {
-        textTransform: "uppercase",
-        flex: 8,
-        paddingBottom:15,
-        paddingTop:15,
-        borderBottomColor: COLOR.GREY_300,
-        borderBottomWidth: 1
-    },
-    locationName: {
-        color: COLOR.TRACKER_GREY,
-        fontWeight: "bold"
-    },
-    icon: {
-        justifyContent: "center",
-        flex: 1,
-        paddingBottom:15,
-        paddingTop:15,
-        borderBottomColor: COLOR.GREY_300,
-        borderBottomWidth: 1
-    }
+const styles = StyleSheet.create({
+  card: {
+    width: '100%',
+    height: 70,
+    backgroundColor: COLOR.WHITE,
+    paddingLeft: 10,
+    paddingRight: 10,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  location: {
+    textTransform: 'uppercase',
+    flex: 8,
+    paddingBottom: 15,
+    paddingTop: 15,
+    borderBottomColor: COLOR.GREY_300,
+    borderBottomWidth: 1
+  },
+  locationName: {
+    color: COLOR.TRACKER_GREY,
+    fontWeight: 'bold'
+  },
+  icon: {
+    justifyContent: 'center',
+    flex: 1,
+    paddingBottom: 15,
+    paddingTop: 15,
+    borderBottomColor: COLOR.GREY_300,
+    borderBottomWidth: 1
+  }
 });
 
 export default styles;
