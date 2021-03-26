14915f2b9d05d2f3439e934a4752a9197ea65b6c
vn50pyy <vn50pyy@homeoffice.wal-mart.com>
2021-03-26 09:30:36 -0500

    Integrated created approvals screen with prepared changes; validated button display limitation and connection.

--- src/navigators/TabNavigator.tsx
+++ src/navigators/TabNavigator.tsx
@@ -2,57 +2,58 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useTypedSelector } from '../state/reducers/RootReducer';
 import { HomeNavigator } from './HomeNavigator';
 import COLOR from '../themes/Color';
 import { strings } from '../locales';
 import { WorklistNavigator } from './WorklistNavigator';
-<<<<<<< HEAD
-import { ApprovalsNavigator } from './ApprovalsNavigator';
-=======
 import { ApprovalListNavigator } from './ApprovalListNavigator';
->>>>>>> development
 
 const Tab = createBottomTabNavigator();
 
-export const TabNavigator = () => (
-  <Tab.Navigator
-    screenOptions={({ route }) => ({
-      tabBarIcon: ({ color, size }) => {
-        if (route.name === strings('HOME.HOME')) {
-          return <MaterialIcons name="home" size={size} color={color} />;
-        } if (route.name === strings('WORKLIST.WORKLIST')) {
-          return <AntDesign name="profile" size={size} color={color} />;
-        } if (route.name === strings('APPROVALS.APPROVALS')) {
-          return <MaterialIcons name="approvals" size={size} color={color} />;
+const TabNavigator = () => {
+  const isManager = useTypedSelector(state => state.User.isManager);
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          if (route.name === strings('HOME.HOME')) {
+            return <MaterialIcons name="home" size={size} color={color} />;
+          } if (route.name === strings('WORKLIST.WORKLIST')) {
+            return <AntDesign name="profile" size={size} color={color} />;
+          } if (route.name === strings('APPROVAL.APPROVAL_ITEMS')) {
+            return <MaterialCommunityIcons name="clipboard-check" size={size} color={color} />;
+          }
+
+          // You can return any component that you like here!
+          return <MaterialIcons name="help" size={size} color={color} />;
         }
+      })}
+      tabBarOptions={{
+        activeTintColor: COLOR.MAIN_THEME_COLOR,
+        inactiveTintColor: COLOR.GREY
+      }}
+    >
+      { /* @ts-ignore */ }
+      <Tab.Screen name={strings('HOME.HOME')} component={HomeNavigator} />
 
-        // You can return any component that you like here!
-        return <MaterialIcons name="help" size={size} color={color} />;
+      { /* @ts-ignore */ }
+      <Tab.Screen
+        name={strings('WORKLIST.WORKLIST')}
+        component={WorklistNavigator}
+      />
+      {isManager
+        ? (
+          <Tab.Screen
+            name={strings('APPROVAL.APPROVAL_ITEMS')} // Needs translation
+            component={ApprovalListNavigator}
+          />
+        ) : <></>
       }
-    })}
-    tabBarOptions={{
-      activeTintColor: COLOR.MAIN_THEME_COLOR,
-      inactiveTintColor: COLOR.GREY
-    }}
-  >
-    { /* @ts-ignore */ }
-    <Tab.Screen name={strings('HOME.HOME')} component={HomeNavigator} />
+    </Tab.Navigator>
+  );
+};
 
-    { /* @ts-ignore */ }
-    <Tab.Screen
-      name={strings('WORKLIST.WORKLIST')}
-      component={WorklistNavigator}
-    />
-<<<<<<< HEAD
-
-    { /* @ts-ignore */ }
-    <Tab.Screen name={strings('APPROVALS.APPROVALS')}
-    component={ApprovalsNavigator} />
-=======
-    <Tab.Screen
-      name={strings('APPROVAL.APPROVAL_ITEMS')} // Needs translation
-      component={ApprovalListNavigator}
-    />
->>>>>>> development
-  </Tab.Navigator>
-);
+export default TabNavigator;
