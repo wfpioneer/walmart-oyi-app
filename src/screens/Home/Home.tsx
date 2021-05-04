ea4711a0a4071b2c0fbd515568812a3038c686c4
vn50j9j <Stephen.Delfaus@walmart.com>
2021-05-04 09:41:46 -0500

    add ts ignore for for require statement

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -34,6 +34,7 @@ const mapDispatchToProps = {
   updateFilterExceptions
 };
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const pkg = require('../../../package.json');
 
 interface HomeScreenProps {
