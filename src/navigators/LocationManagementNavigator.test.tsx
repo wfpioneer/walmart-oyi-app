af698920472cc1fadbac854f077df17808da0dec
Ian Stansell <ian.stansell@walmart.com>
2021-06-13 08:58:03 -0500

    Add snapshot test for LocationManagement Navigator

--- src/navigators/LocationManagementNavigator.test.tsx
+++ src/navigators/LocationManagementNavigator.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
+import { LocationManagementNavigatorStack } from './LocationManagementNavigator';
 
 jest.mock('../utils/AppCenterTool.ts', () => jest.requireActual('../utils/__mocks__/AppCenterTool'));
 jest.mock('../utils/sessionTimeout.ts', () => jest.requireActual('../utils/__mocks__/sessTimeout'));
 
 describe('LocationManagement Navigator', () => {
+  it('Renders the LocationManagement Navigator', () => {
+    const renderer = ShallowRenderer.createRenderer();
 
+    renderer.render(
+      <LocationManagementNavigatorStack />
+    );
+
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
+  });
 });
