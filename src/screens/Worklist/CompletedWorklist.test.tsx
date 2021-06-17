0dcb5ada18006e0c2d702919350cdbeb58401356
vn50j9j <Stephen.Delfaus@walmart.com>
2021-06-17 10:14:02 -0500

    refactor tests to match TodoWorklist tests nearly identically
    * These can be combined into a single component with a single set of tests

--- src/screens/Worklist/CompletedWorklist.test.tsx
+++ src/screens/Worklist/CompletedWorklist.test.tsx
@@ -1,76 +1,129 @@
 import React from 'react';
-import * as redux from 'react-redux';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import { NavigationProp } from '@react-navigation/native';
-import { CompletedWorklist } from './CompletedWorklist';
 import { mockWorkListComplete, mockWorkListToDo } from '../../mockData/mockWorkList';
+import { CompletedWorklistScreen } from './CompletedWorklist';
 
 jest.mock('../../utils/AppCenterTool', () => jest.requireActual('../../utils/__mocks__/AppCenterTool'));
 jest.mock('../../utils/sessionTimeout.ts', () => jest.requireActual('../../utils/__mocks__/sessTimeout'));
-
 let navigationProp: NavigationProp<any>;
-const mockGroupToggle = false;
-
-const mockUseTypedSelectorData = {
-  isWaiting: false,
-  result: { data: mockWorkListToDo },
-  error: null,
-  filterExceptions: [],
-  filterCategories: []
-};
-
-const setState = jest.fn();
-
-const mockUseStateData: any = (initState: any) => [
-  initState,
-  setState
-];
-
-const spyOnUseSelector = jest.spyOn(redux, 'useSelector');
-const spyOnUseState = jest.spyOn(React, 'useState');
-
-spyOnUseState.mockReturnValue(mockUseStateData);
-
 describe('CompletedWorklistScreen', () => {
   describe('Tests rendering Worklist data', () => {
-    it('Renders no items for a worklist that contains only incomplete items', () => {
-      spyOnUseSelector.mockReturnValue(mockUseTypedSelectorData);
+    it('Renders no worklist items when all are incomplete', () => {
       const renderer = ShallowRenderer.createRenderer();
+      const toDoWorklistResult = {
+        data: mockWorkListToDo
+      };
       renderer.render(
-        <CompletedWorklist />
+        <CompletedWorklistScreen
+          isWaiting={false}
+          result={toDoWorklistResult}
+          error={null}
+          dispatch={jest.fn()}
+          filterCategories={[]}
+          filterExceptions={[]}
+          groupToggle={false}
+          updateGroupToggle={jest.fn()}
+          navigation={navigationProp}
+        />
       );
       expect(renderer.getRenderOutput()).toMatchSnapshot();
     });
-
-    it('Renders all items for a worklist that contains only complete items', () => {
-      spyOnUseSelector.mockReturnValue({ ...mockUseTypedSelectorData, result: { data: mockWorkListComplete } });
+    it('Renders every worklist item when all are complete', () => {
       const renderer = ShallowRenderer.createRenderer();
+      const completeWorklistResult = {
+        data: mockWorkListComplete
+      };
       renderer.render(
-        <CompletedWorklist />
+        <CompletedWorklistScreen
+          isWaiting={false}
+          result={completeWorklistResult}
+          error={null}
+          dispatch={jest.fn()}
+          filterCategories={[]}
+          filterExceptions={[]}
+          groupToggle={false}
+          updateGroupToggle={jest.fn()}
+          navigation={navigationProp}
+        />
       );
       expect(renderer.getRenderOutput()).toMatchSnapshot();
     });
-
-    it('Renders only complete items for a worklist that has complete and incomplete items', () => {
-      spyOnUseSelector.mockReturnValue({
-        ...mockUseTypedSelectorData,
-        result: { data: [...mockWorkListComplete, ...mockWorkListToDo] }
-      });
+    it('Renders only complete worklist items for a Worklist with incomplete & complete items', () => {
       const renderer = ShallowRenderer.createRenderer();
+      const mixedWorklistResult = {
+        data: [...mockWorkListToDo, ...mockWorkListComplete]
+      };
       renderer.render(
-        <CompletedWorklist />
+        <CompletedWorklistScreen
+          isWaiting={false}
+          result={mixedWorklistResult}
+          error={null}
+          dispatch={jest.fn()}
+          filterCategories={[]}
+          filterExceptions={[]}
+          groupToggle={false}
+          updateGroupToggle={jest.fn()}
+          navigation={navigationProp}
+        />
       );
       expect(renderer.getRenderOutput()).toMatchSnapshot();
     });
-
-    it('Renders no items for an empty list', () => {
-      spyOnUseSelector.mockReturnValue({
-        ...mockUseTypedSelectorData,
-        result: { data: [] }
-      });
+    it('Renders no worklist items when there are none', () => {
       const renderer = ShallowRenderer.createRenderer();
+      const emptyWorklistResult = {
+        data: []
+      };
       renderer.render(
-          <CompletedWorklist />
+        <CompletedWorklistScreen
+          isWaiting={false}
+          result={emptyWorklistResult}
+          error={null}
+          dispatch={jest.fn()}
+          filterCategories={[]}
+          filterExceptions={[]}
+          groupToggle={false}
+          updateGroupToggle={jest.fn()}
+          navigation={navigationProp}
+        />
+      );
+      expect(renderer.getRenderOutput()).toMatchSnapshot();
+    });
+  });
+  describe('Tests rendering worklist api responses', () => {
+    it('Renders error message for failed worklist request', () => {
+      const renderer = ShallowRenderer.createRenderer();
+
+      renderer.render(
+        <CompletedWorklistScreen
+          isWaiting={false}
+          result={null}
+          error="Network Error"
+          dispatch={jest.fn()}
+          filterCategories={[]}
+          filterExceptions={[]}
+          groupToggle={false}
+          updateGroupToggle={jest.fn()}
+          navigation={navigationProp}
+        />
+      );
+      expect(renderer.getRenderOutput()).toMatchSnapshot();
+    });
+    it('Renders refresh set to true when waiting for worklist api response', () => {
+      const renderer = ShallowRenderer.createRenderer();
+
+      renderer.render(
+        <CompletedWorklistScreen
+          isWaiting={true}
+          result={null}
+          error={null}
+          dispatch={jest.fn()}
+          filterCategories={[]}
+          filterExceptions={[]}
+          groupToggle={false}
+          updateGroupToggle={jest.fn()}
+          navigation={navigationProp}
+        />
       );
       expect(renderer.getRenderOutput()).toMatchSnapshot();
     });
