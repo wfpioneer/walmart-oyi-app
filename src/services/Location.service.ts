94252a712474fd9e698d7dcb02c6c531d67779ae
Jyoti D Rozario <j0d0d3g@m-c02fm5vfmd6r.homeoffice.wal-mart.com>
2021-07-11 21:15:51 -0400

    renamed service file

--- src/services/Location.service.ts
+++ src/services/Location.service.ts
@@ -2,7 +2,7 @@ import { AxiosResponse } from 'axios';
 import Request from './Request';
 import { Environment, getEnvironment } from '../utils/environment';
 
-export default class GetAllZones {
+export default class LocationService {
   public static getAllZones() : Promise<AxiosResponse<unknown>> {
     const urls: Environment = getEnvironment();
     return Request.get(
