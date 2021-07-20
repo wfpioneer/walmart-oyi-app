6349fd4974b308c1b3b5cbeaa580519a6758d92f
Stefan Small <vn50pz2@homeoffice.wal-mart.com>
2021-07-20 15:30:16 -0400

    remove unused parameters from test files

--- src/screens/PrintQueue/PrintQueue.test.tsx
+++ src/screens/PrintQueue/PrintQueue.test.tsx
@@ -50,12 +50,8 @@ describe('PrintQueueScreen', () => {
         route={routeProp}
         itemIndexToEdit={-1}
         setItemIndexToEdit={jest.fn()}
-        apiInProgress={false}
-        setAPIInProgress={jest.fn()}
         error={defaultError}
         setError={jest.fn()}
-        apiStart={0}
-        setApiStart={jest.fn()}
         trackEventCall={jest.fn()}
         validateSessionCall={jest.fn(() => Promise.resolve())}
         useEffectHook={jest.fn()}
@@ -154,12 +150,8 @@ describe('PrintQueueScreen', () => {
         route={routeProp}
         itemIndexToEdit={-1}
         setItemIndexToEdit={jest.fn()}
-        apiInProgress={false}
-        setAPIInProgress={jest.fn()}
         error={defaultError}
         setError={jest.fn()}
-        apiStart={0}
-        setApiStart={jest.fn()}
         trackEventCall={jest.fn()}
         validateSessionCall={jest.fn(() => Promise.resolve())}
         useEffectHook={jest.fn()}
@@ -187,12 +179,8 @@ describe('PrintQueueScreen', () => {
         route={routeProp}
         itemIndexToEdit={singleItemToEdit}
         setItemIndexToEdit={jest.fn()}
-        apiInProgress={false}
-        setAPIInProgress={jest.fn()}
         error={defaultError}
         setError={jest.fn()}
-        apiStart={0}
-        setApiStart={jest.fn()}
         trackEventCall={jest.fn()}
         validateSessionCall={jest.fn(() => Promise.resolve())}
         useEffectHook={jest.fn()}
@@ -217,12 +205,8 @@ describe('PrintQueueScreen', () => {
         route={routeProp}
         itemIndexToEdit={-1}
         setItemIndexToEdit={jest.fn()}
-        apiInProgress={false}
-        setAPIInProgress={jest.fn()}
         error={printError}
         setError={jest.fn()}
-        apiStart={0}
-        setApiStart={jest.fn()}
         trackEventCall={jest.fn()}
         validateSessionCall={jest.fn(() => Promise.resolve())}
         useEffectHook={jest.fn()}
@@ -247,12 +231,8 @@ describe('PrintQueueScreen', () => {
         route={routeProp}
         itemIndexToEdit={-1}
         setItemIndexToEdit={jest.fn()}
-        apiInProgress={true}
-        setAPIInProgress={jest.fn()}
         error={defaultError}
         setError={jest.fn()}
-        apiStart={0}
-        setApiStart={jest.fn()}
         trackEventCall={jest.fn()}
         validateSessionCall={jest.fn(() => Promise.resolve())}
         useEffectHook={jest.fn()}
@@ -270,7 +250,6 @@ describe('PrintQueueScreen', () => {
         printQueue: [],
         route: { key: '', name: 'TEST' },
         selectedPrinter: defaultPrinter,
-        setApiStart: jest.fn(),
         trackEventCall
       });
 
@@ -285,7 +264,6 @@ describe('PrintQueueScreen', () => {
         printQueue: [],
         route: { key: '', name: 'TEST' },
         selectedPrinter: defaultPrinter,
-        setApiStart: jest.fn(),
         trackEventCall: jest.fn()
       });
 
@@ -300,26 +278,10 @@ describe('PrintQueueScreen', () => {
         printQueue: [],
         route: { key: '', name: 'TEST' },
         selectedPrinter: defaultPrinter,
-        setApiStart: jest.fn(),
         trackEventCall: jest.fn()
       });
 
       expect(validateSessionCall).toHaveBeenCalled();
-    });
-    it('calls handlePrint setApiStart', async () => {
-      const setApiStart = jest.fn();
-      await handlePrint({
-        validateSessionCall: jest.fn(() => Promise.resolve()),
-        dispatch: jest.fn(),
-        navigation: navigationProp,
-        printQueue: [],
-        route: { key: '', name: 'TEST' },
-        selectedPrinter: defaultPrinter,
-        setApiStart,
-        trackEventCall: jest.fn()
-      });
-
-      expect(setApiStart).toHaveBeenCalled();
     });
   });
 });
