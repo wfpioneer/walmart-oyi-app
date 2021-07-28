6600eaf47134b36becacb50208aa9a41b715164d
Stefan Small <vn50pz2@homeoffice.wal-mart.com>
2021-07-28 19:03:56 -0400

    check if siteId is undefined in appCenterTrack Event

--- src/state/reducers/User.ts
+++ src/state/reducers/User.ts
@@ -23,7 +23,6 @@ export const UserReducer = (state = initialState, action: Actions): User => {
     case USER_LOGIN:
       return {
         ...action.payload,
-        siteId: action.payload.siteId ?? 0,
         features: []
       };
     case USER_LOGOUT:
