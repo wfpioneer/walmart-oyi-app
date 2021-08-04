d78b5a2e27d171ade0a26451b02b88e0f2b1509d
Jyoti D Rozario <j0d0d3g@m-c02fm5vfmd6r.homeoffice.wal-mart.com>
2021-08-04 12:59:11 -0400

    applied changes

--- src/mockData/sectionDetails.ts
+++ src/mockData/sectionDetails.ts
@@ -3,26 +3,26 @@ import { SectionItem } from '../models/LocationItems';
 export const mockSections: SectionItem[] = [
   {
     sectionId: 1,
-    sectionName: 'Section 1-1'
+    sectionName: 'Section ABAR1-1'
   },
   {
     sectionId: 2,
-    sectionName: 'Section 1-2'
+    sectionName: 'Section ABAR1-2'
   },
   {
     sectionId: 3,
-    sectionName: 'Section 1-3'
+    sectionName: 'Section ABAR1-3'
   },
   {
     sectionId: 4,
-    sectionName: 'Section 1-4'
+    sectionName: 'Section ABAR1-4'
   },
   {
     sectionId: 5,
-    sectionName: 'Section 1-5'
+    sectionName: 'Section ABAR1-5'
   },
   {
     sectionId: 6,
-    sectionName: 'Section 1-6'
+    sectionName: 'Section ABAR1-6'
   }
 ];
