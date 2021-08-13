5395b9a10f11c2a9b00a2da9f0b1c691b8287e65
Jyoti D Rozario <j0d0d3g@m-c02fm5vfmd6r.homeoffice.wal-mart.com>
2021-08-13 15:11:20 -0400

    updated snapshot tests

--- src/components/LocationTabs/LocationTabs.test.tsx
+++ src/components/LocationTabs/LocationTabs.test.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import LocationTabs from './LocationTabs';
-import { mockLocationDetails } from '../../mockData/locationDetails';
+import {
+  mockLocationDetails,
+  mockLocationDetailsEmpty,
+  mockLocationDetailsLargeLocationCount
+} from '../../mockData/locationDetails';
 
-describe('Test Location Details Screen', () => {
-  it('Renders Location Details Screen with Mock Data', () => {
+describe('Test Location Tabs', () => {
+  it('Renders Location Tabs with Mock Data', () => {
     const renderer = ShallowRenderer.createRenderer();
     renderer.render(
       <LocationTabs
         mockData={mockLocationDetails}
+      />
+    );
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
+  });
+});
+
+describe('Test Location Tabs', () => {
+  it('Renders Location Tabs with Mock Empty Data', () => {
+    const renderer = ShallowRenderer.createRenderer();
+    renderer.render(
+      <LocationTabs
+        mockData={mockLocationDetailsEmpty}
+      />
+    );
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
+  });
+});
+
+describe('Test Location Tabs', () => {
+  it('Renders Location Tabs with Mock Large Location Data', () => {
+    const renderer = ShallowRenderer.createRenderer();
+    renderer.render(
+      <LocationTabs
+        mockData={mockLocationDetailsLargeLocationCount}
       />
     );
     expect(renderer.getRenderOutput()).toMatchSnapshot();
