34b7b66e1e405b9ab2539102a5c20fe6aec7b74f
Stephen Delfaus <s0d08wb@homeoffice.wal-mart.com>
2021-09-15 15:45:45 -0400

    fix ItemDetailsList.style eslint issues

--- src/components/ItemDetailsList/ItemDetailsList.style.ts
+++ src/components/ItemDetailsList/ItemDetailsList.style.ts
@@ -1,16 +1,16 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 import COLOR from '../../themes/Color';
 
 const styles = StyleSheet.create({
   listContainer: {
-    paddingHorizontal: 10,
+    paddingHorizontal: 10
   },
   listRowContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     paddingVertical: 12,
     paddingHorizontal: 10,
-    borderTopColor: COLOR.GREY_300,
+    borderTopColor: COLOR.GREY_300
   },
   additionalInfoContainer: {
     flexDirection: 'row',
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
   },
   infoIcon: {
     paddingRight: 6
-  },
+  }
 });
 
 export default styles;
