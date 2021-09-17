99174f2c822e414f9091d4642c32801ee3d94b43
Stephen Delfaus <s0d08wb@homeoffice.wal-mart.com>
2021-09-17 10:00:43 -0400

    add missing lines to new tests, add generated snapshots

--- src/components/ItemDetailsList/ItemDetailsList.test.tsx
+++ src/components/ItemDetailsList/ItemDetailsList.test.tsx
@@ -14,22 +14,26 @@ describe('ItemDetailsList Component', () => {
   it('should display an additional note below rows with that property defined', () => {
     const renderer = ShallowRenderer.createRenderer();
     renderer.render(<ItemDetailsList rows={mockItemDetailsListRowsAdditionalNote} />);
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
   });
 
   it('should render rows with matching style when additionalNote and indentAfterFirstRow are not defined', () => {
     const renderer = ShallowRenderer.createRenderer();
     renderer.render(<ItemDetailsList rows={mockItemDetailsListRowsNoNote} />);
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
   });
 
   describe('indenting behavior', () => {
     it('should indent all rows after the first if indentAfterFirstRow is true', () => {
       const renderer = ShallowRenderer.createRenderer();
       renderer.render(<ItemDetailsList rows={mockItemDetailsListRowsNoNote} indentAfterFirstRow={true} />);
+      expect(renderer.getRenderOutput()).toMatchSnapshot();
     });
 
     it('should indent all rows after the first if indentAfterFirstRow is true regardless of additionalNotes', () => {
       const renderer = ShallowRenderer.createRenderer();
       renderer.render(<ItemDetailsList rows={mockItemDetailsListRowsAdditionalNote} indentAfterFirstRow={true} />);
+      expect(renderer.getRenderOutput()).toMatchSnapshot();
     });
 
     it('should only indent labels and not additionalNotes', () => {
@@ -37,16 +41,19 @@ describe('ItemDetailsList Component', () => {
       const mockItemDetailsRowsWithNotes = mockItemDetailsListRowsNoNote
         .map(row => ({ ...row, additionalNote: 'An Additional Note' }));
       renderer.render(<ItemDetailsList rows={mockItemDetailsRowsWithNotes} indentAfterFirstRow={true} />);
+      expect(renderer.getRenderOutput()).toMatchSnapshot();
     });
 
     it('should not indent anything if indentAfterFirstRow is not specified', () => {
       const renderer = ShallowRenderer.createRenderer();
       renderer.render(<ItemDetailsList rows={mockItemDetailsListRowsNoNote} />);
+      expect(renderer.getRenderOutput()).toMatchSnapshot();
     });
 
     it('should not indent anything if indentAfterFirstRow is false', () => {
       const renderer = ShallowRenderer.createRenderer();
       renderer.render(<ItemDetailsList rows={mockItemDetailsListRowsNoNote} indentAfterFirstRow={false} />);
+      expect(renderer.getRenderOutput()).toMatchSnapshot();
     });
   });
 });
