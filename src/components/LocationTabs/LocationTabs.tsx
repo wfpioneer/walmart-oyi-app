e4ea0e5485d50d8bd4340fd6c0cd29f7c1084805
Stephen Delfaus <s0d08wb@homeoffice.wal-mart.com>
2021-10-04 13:36:19 -0400

    comment out write functionality

--- src/components/LocationTabs/LocationTabs.tsx
+++ src/components/LocationTabs/LocationTabs.tsx
@@ -20,17 +20,17 @@ const ItemHeader = () : JSX.Element => (
     <Text style={styles.tabHeaderText}>
       {strings('LOCATION.ITEMS')}
     </Text>
-    <TouchableOpacity>
-      <Text style={styles.clear}>
-        {strings('LOCATION.CLEAR_ALL')}
-      </Text>
-    </TouchableOpacity>
-    <Text style={styles.pipe}>|</Text>
-    <TouchableOpacity>
-      <Text style={styles.add}>
-        {strings('LOCATION.ADD')}
-      </Text>
-    </TouchableOpacity>
+    {/* <TouchableOpacity> */}
+    {/*  <Text style={styles.clear}> */}
+    {/*    {strings('LOCATION.CLEAR_ALL')} */}
+    {/*  </Text> */}
+    {/* </TouchableOpacity> */}
+    {/* <Text style={styles.pipe}>|</Text> */}
+    {/* <TouchableOpacity> */}
+    {/*  <Text style={styles.add}> */}
+    {/*    {strings('LOCATION.ADD')} */}
+    {/*  </Text> */}
+    {/* </TouchableOpacity> */}
   </View>
 );
 
