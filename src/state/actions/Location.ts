d232bef653cf053024b42d4ad72f6e6d38c9ea8a
t0s00og <Timothy.Strickland@walmart.com>
2021-10-28 10:19:14 -0500

    moved item related data from location to itemDetailsScreen in redux
    adjusted effected components in regards to new redux format
    
    adjusted effected tests

--- src/state/actions/Location.ts
+++ src/state/actions/Location.ts
@@ -1,74 +1,6 @@
-import Location from '../../models/Location';
-
-export const SET_FLOOR_LOCATIONS = 'LOCATION/SET_FLOOR_LOCATIONS';
-export const SET_RESERVE_LOCATIONS = 'LOCATION/SET_RESERVE_LOCATIONS';
-export const SET_ITEM_LOC_DETAILS = 'LOCATION/SET_ITEM_LOC_DETAILS';
-export const ADD_LOCATION_TO_EXISTING = 'LOCATION/ADD_LOCATION_TO_EXISTING';
-export const EDIT_EXISTING_LOCATION = 'LOCATION/EDIT_EXISTING_LOCATION';
-export const DELETE_LOCATION_FROM_EXISTING = 'LOCATION/DELETE_LOCATION_FROM_EXISTING';
-export const RESET_LOCATIONS = 'LOCATION/RESET_LOCATIONS';
-export const GET_LOCATION_DETAILS = 'LOCATION/GET_LOCATION_DETAILS';
 export const SELECT_ZONE = 'LOCATION/SELECT_ZONE';
 export const SELECT_AISLE = 'LOCATION/SELECT_AISLE';
 export const SELECT_SECTION = 'LOCATION/SELECT_SECTION';
-
-export const setItemLocDetails = (itemNbr: number, upcNbr: string, exceptionType: string) => ({
-  type: SET_ITEM_LOC_DETAILS,
-  payload: {
-    itemNbr,
-    upcNbr,
-    exceptionType
-  }
-} as const);
-
-export const setFloorLocations = (floor: Location[]) => ({
-  type: SET_FLOOR_LOCATIONS,
-  payload: floor
-} as const);
-
-export const setReserveLocations = (reserve: Location[]) => ({
-  type: SET_RESERVE_LOCATIONS,
-  payload: reserve
-} as const);
-
-export const addLocationToExisting = (locationName: string, locationTypeNbr: number, locationArea: string) => ({
-  type: ADD_LOCATION_TO_EXISTING,
-  payload: {
-    locationName,
-    locationTypeNbr,
-    locationArea
-  }
-} as const);
-
-export const editExistingLocation = (locationName: string, locationTypeNbr: number, locationArea: string,
-  locIndex: number) => ({
-  type: EDIT_EXISTING_LOCATION,
-  payload: {
-    locationName,
-    locationTypeNbr,
-    locationArea,
-    locIndex
-  }
-} as const);
-
-export const deleteLocationFromExisting = (locationArea: string, locIndex: number) => ({
-  type: DELETE_LOCATION_FROM_EXISTING,
-  payload: {
-    locationArea,
-    locIndex
-  }
-} as const);
-
-export const resetLocations = () => ({
-  type: RESET_LOCATIONS
-} as const);
-
-export const getLocationDetails = (itemNbr: number) => ({
-  type: GET_LOCATION_DETAILS,
-  payload: {
-    itemNbr
-  }
-} as const);
 
 export const selectZone = (id: number, name: string) => ({
   type: SELECT_ZONE,
@@ -95,14 +27,6 @@ export const selectSection = (id: number, name: string) => ({
 } as const);
 
 export type Actions =
-  | ReturnType<typeof setItemLocDetails>
-  | ReturnType<typeof setFloorLocations>
-  | ReturnType<typeof setReserveLocations>
-  | ReturnType<typeof addLocationToExisting>
-  | ReturnType<typeof editExistingLocation>
-  | ReturnType<typeof deleteLocationFromExisting>
-  | ReturnType<typeof getLocationDetails>
-  | ReturnType<typeof resetLocations>
-  | ReturnType<typeof selectZone>
+  ReturnType<typeof selectZone>
   | ReturnType<typeof selectAisle>
   | ReturnType<typeof selectSection>;
--- src/state/reducers/Location.ts
+++ src/state/reducers/Location.ts
@@ -1,26 +1,11 @@
 import {
-  ADD_LOCATION_TO_EXISTING,
   Actions,
-  DELETE_LOCATION_FROM_EXISTING,
-  EDIT_EXISTING_LOCATION,
-  RESET_LOCATIONS,
   SELECT_AISLE,
   SELECT_SECTION,
   SELECT_ZONE,
-  SET_FLOOR_LOCATIONS,
-  SET_ITEM_LOC_DETAILS,
-  SET_RESERVE_LOCATIONS
 } from '../actions/Location';
-import LocationType from '../../models/Location';
 
 interface LocationState {
-  floorLocations: Array<LocationType>;
-  reserveLocations: Array<LocationType>;
-  itemLocDetails: {
-    itemNbr: number;
-    upcNbr: string;
-    exceptionType: string;
-  };
   selectedZone: {
     id: number;
     name: string;
@@ -36,13 +21,6 @@ interface LocationState {
 }
 
 const initialState: LocationState = {
-  floorLocations: [],
-  reserveLocations: [],
-  itemLocDetails: {
-    itemNbr: 0,
-    upcNbr: '',
-    exceptionType: ''
-  },
   selectedZone: {
     id: 0,
     name: ''
@@ -62,132 +40,6 @@ export const Location = (
   action: Actions
 ) : LocationState => {
   switch (action.type) {
-    case SET_ITEM_LOC_DETAILS:
-      return {
-        ...state,
-        itemLocDetails: { ...action.payload }
-      };
-    case SET_FLOOR_LOCATIONS:
-      return {
-        ...state,
-        floorLocations: action.payload.map((loc: LocationType) => ({
-          ...loc,
-          locationName: `${loc.zoneName}${loc.aisleName}-${loc.sectionName}`
-        }))
-      };
-    case SET_RESERVE_LOCATIONS:
-      return {
-        ...state,
-        reserveLocations: action.payload.map((loc: LocationType) => ({
-          ...loc,
-          locationName: `${loc.zoneName}${loc.aisleName}-${loc.sectionName}`
-        }))
-      };
-    case ADD_LOCATION_TO_EXISTING:
-      if (action.payload.locationArea === 'floor') {
-        const addFloorLocations = [...state.floorLocations];
-        addFloorLocations.push({
-          zoneId: 0,
-          aisleId: 0,
-          sectionId: 0,
-          zoneName: '',
-          aisleName: '',
-          sectionName: '',
-          locationName: action.payload.locationName,
-          type: '',
-          typeNbr: action.payload.locationTypeNbr
-        });
-        return {
-          ...state,
-          floorLocations: addFloorLocations
-        };
-      }
-      if (action.payload.locationArea === 'reserve') {
-        const addReserveLocations = [...state.reserveLocations];
-        addReserveLocations.push({
-          zoneId: 0,
-          aisleId: 0,
-          sectionId: 0,
-          zoneName: '',
-          aisleName: '',
-          sectionName: '',
-          locationName: action.payload.locationName,
-          type: '',
-          typeNbr: action.payload.locationTypeNbr
-        });
-        return {
-          ...state,
-          reserveLocations: addReserveLocations
-        };
-      }
-      return {
-        ...state
-      };
-    case EDIT_EXISTING_LOCATION:
-      if (action.payload.locationArea === 'floor') {
-        const editedLocation = {
-          zoneId: 0,
-          aisleId: 0,
-          sectionId: 0,
-          zoneName: '',
-          aisleName: '',
-          sectionName: '',
-          locationName: action.payload.locationName,
-          type: '',
-          typeNbr: action.payload.locationTypeNbr
-        };
-        const editFloorLocations = [...state.floorLocations].splice(action.payload.locIndex, 1, editedLocation);
-        return {
-          ...state,
-          floorLocations: editFloorLocations
-        };
-      }
-      if (action.payload.locationArea === 'reserve') {
-        const editedLocation = {
-          zoneId: 0,
-          aisleId: 0,
-          sectionId: 0,
-          zoneName: '',
-          aisleName: '',
-          sectionName: '',
-          locationName: action.payload.locationName,
-          type: '',
-          typeNbr: action.payload.locationTypeNbr
-        };
-        const editReserveLocations = [...state.reserveLocations].splice(action.payload.locIndex, 1, editedLocation);
-        return {
-          ...state,
-          reserveLocations: editReserveLocations
-        };
-      }
-      return {
-        ...state
-      };
-    case DELETE_LOCATION_FROM_EXISTING: {
-      const { locIndex } = action.payload;
-
-      if (action.payload.locationArea === 'floor') {
-        const deleteFloorLocation = [
-          ...state.floorLocations.slice(0, locIndex),
-          ...state.floorLocations.slice(locIndex + 1)];
-        return {
-          ...state,
-          floorLocations: deleteFloorLocation
-        };
-      }
-      if (action.payload.locationArea === 'reserve') {
-        const deleteReserveLocation = [
-          ...state.reserveLocations.slice(0, locIndex),
-          ...state.reserveLocations.slice(locIndex + 1)];
-        return {
-          ...state,
-          reserveLocations: deleteReserveLocation
-        };
-      }
-      return {
-        ...state
-      };
-    }
     case SELECT_ZONE: {
       return {
         ...state,
@@ -215,8 +67,6 @@ export const Location = (
         }
       };
     }
-    case RESET_LOCATIONS:
-      return initialState;
     default:
       return state;
   }
