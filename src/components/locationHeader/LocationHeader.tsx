e9df80d9e171f11499dcc45e7ed9a5c08eb30f18
Kyle Welch <kyle.welch@walmart.com>
2021-11-15 10:47:17 -0600

    Added button for print to section details location header

--- src/components/locationHeader/LocationHeader.tsx
+++ src/components/locationHeader/LocationHeader.tsx
@@ -2,25 +2,45 @@ import React from 'react';
 import {
   Text, View
 } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import styles from './LocationHeader.style';
 
 interface LocationHeaderProps {
-    location: string,
-    details: string
+  location: string,
+  details: string,
+  buttonText?: string,
+  buttonPress?: () => void
 }
 
 export const LocationHeader = (props: LocationHeaderProps) : JSX.Element => {
-  const { location, details } = props;
+  const {
+    location, details, buttonText, buttonPress
+  } = props;
+
+  const buttonView = () => (buttonText ? (
+    <TouchableOpacity onPress={buttonPress}>
+      <Text style={styles.buttonText}>{buttonText}</Text>
+    </TouchableOpacity>
+  ) : null);
+
   return (
-    <View style={styles.staticHeader}>
-      <Text>
-        {location}
-      </Text>
-      <Text style={styles.detailsText}>
-        {details}
-      </Text>
+    <View style={styles.headerContainer}>
+      <View style={styles.headerText}>
+        <Text>
+          {location}
+        </Text>
+        <Text style={styles.detailsText}>
+          {details}
+        </Text>
+      </View>
+      {buttonView()}
     </View>
   );
+};
+
+LocationHeader.defaultProps = {
+  buttonText: '',
+  buttonPress: undefined
 };
 
 export default LocationHeader;
