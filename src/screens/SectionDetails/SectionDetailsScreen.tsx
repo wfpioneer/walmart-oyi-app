8e3bfac1fd800481fe3f19c5544f29b9a99a6a8c
Stefan Small <vn50pz2@homeoffice.wal-mart.com>
2021-11-24 15:49:59 -0500

    navigate to itemdetails from section details screen

--- src/screens/SectionDetails/SectionDetailsScreen.tsx
+++ src/screens/SectionDetails/SectionDetailsScreen.tsx
@@ -64,7 +64,7 @@ export const SectionDetailsScreen = (props: SectionDetailsProps) : JSX.Element =
         dispatch(selectSection(section.id, section.name));
       }
     }
-  });
+  }, []);
   const locationItem: LocationItem | undefined = (getSectionDetailsApi.result && getSectionDetailsApi.result.data);
 
   if (getSectionDetailsApi.isWaiting) {
@@ -102,7 +102,14 @@ export const SectionDetailsScreen = (props: SectionDetailsProps) : JSX.Element =
       <FlatList
         data={route.name === 'FloorDetails' ? locationItem?.floor : locationItem?.reserve}
         renderItem={({ item }) => (
-          route.name === 'FloorDetails' ? <FloorItemRow item={item} /> : <ReservePalletRow reservePallet={item} />
+          route.name === 'FloorDetails'
+            ? (
+              <FloorItemRow
+                item={item}
+                dispatch={dispatch}
+                navigation={navigation}
+              />
+            ) : <ReservePalletRow reservePallet={item} />
         )}
         keyExtractor={(item, idx) => `${item.itemNbr}${idx}`}
       />
