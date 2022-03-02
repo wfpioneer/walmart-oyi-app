b8902c829072f03eda80ce897af60112c9696957
Ravivarman Thayumanavan <Ravi.Varman.Thayumanavan@walmart.com>
2022-03-02 18:55:53 +0530

    Added API integration for scanning pallet in Binning Screen

--- src/screens/Binning/Binning.test.tsx
+++ src/screens/Binning/Binning.test.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import { NavigationProp } from '@react-navigation/native';
+import { NavigationProp, RouteProp } from '@react-navigation/native';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import {
   BinningScreen, Pallet, binningItemCard
 } from './Binning';
+import { AsyncState } from '../../models/AsyncState';
 import { mockPallets } from '../../mockData/binning';
 
-let navigationProp: NavigationProp<any>;
+jest.mock('../../state/actions/Modal', () => ({
+  showActivityModal: jest.fn(),
+  hideActivityModal: jest.fn()
+}));
+
+const defaultAsyncState: AsyncState = {
+  isWaiting: false,
+  value: null,
+  error: null,
+  result: null
+};
+const navigationProp: NavigationProp<any> = {
+  addListener: jest.fn(),
+  canGoBack: jest.fn(),
+  dangerouslyGetParent: jest.fn(),
+  dangerouslyGetState: jest.fn(),
+  dispatch: jest.fn(),
+  goBack: jest.fn(),
+  isFocused: jest.fn(() => true),
+  removeListener: jest.fn(),
+  reset: jest.fn(),
+  setOptions: jest.fn(),
+  setParams: jest.fn(),
+  navigate: jest.fn()
+};
+
+let routeProp: RouteProp<any, string>;
 
 describe('BinningScreen', () => {
   describe('Tests rendering the BinningScreen component', () => {
@@ -18,6 +45,9 @@ describe('BinningScreen', () => {
           navigation={navigationProp}
           dispatch={jest.fn}
           isManualScanEnabled={true}
+          useEffectHook={jest.fn}
+          route={routeProp}
+          getPalletApi={defaultAsyncState}
         />
       );
       expect(renderer.getRenderOutput()).toMatchSnapshot();
@@ -31,6 +61,41 @@ describe('BinningScreen', () => {
           navigation={navigationProp}
           dispatch={jest.fn}
           isManualScanEnabled={true}
+          useEffectHook={jest.fn}
+          route={routeProp}
+          getPalletApi={defaultAsyncState}
+        />
+      );
+      expect(renderer.getRenderOutput()).toMatchSnapshot();
+    });
+
+    it('Test renders the BinningScreen with Successful getPalletApi state', () => {
+      const sucessAsyncState: AsyncState = {
+        isWaiting: false,
+        value: null,
+        error: null,
+        result: {
+          palletId: 63,
+          expirationDate: '3/3/2022',
+          items: [
+            {
+              itemDesc: 'itemDesc',
+              price: '69.00',
+              upcNbr: '75763861473.000000',
+              quantity: 1
+            }]
+        }
+      };
+      const renderer = ShallowRenderer.createRenderer();
+      renderer.render(
+        <BinningScreen
+          pallets={mockPallets}
+          navigation={navigationProp}
+          dispatch={jest.fn}
+          isManualScanEnabled={true}
+          useEffectHook={jest.fn}
+          route={routeProp}
+          getPalletApi={sucessAsyncState}
         />
       );
       expect(renderer.getRenderOutput()).toMatchSnapshot();
--- src/screens/Binning/Binning.tsx
+++ src/screens/Binning/Binning.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { EffectCallback, useEffect } from 'react';
 import {
-  Text, TouchableOpacity, View
+  EmitterSubscription, Keyboard, KeyboardAvoidingView, Text, TouchableOpacity, View
 } from 'react-native';
+import { trackEvent } from 'appcenter-analytics';
 import { FlatList } from 'react-native-gesture-handler';
 import { useDispatch } from 'react-redux';
 import { Dispatch } from 'redux';
-import { NavigationProp, useNavigation } from '@react-navigation/native';
+import Toast from 'react-native-toast-message';
+import {
+  NavigationProp, RouteProp, useNavigation, useRoute
+} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { openCamera } from '../../utils/scannerUtils';
+import { barcodeEmitter, openCamera } from '../../utils/scannerUtils';
+import { validateSession } from '../../utils/sessionTimeout';
 import { strings } from '../../locales';
 import COLOR from '../../themes/Color';
 import styles from './Binning.style';
 import Button from '../../components/buttons/Button';
 import ManualScan from '../../components/manualscan/ManualScan';
 import { useTypedSelector } from '../../state/reducers/RootReducer';
+import {
+  getPalletInfo
+} from '../../state/actions/saga';
+import {
+  GET_PALLET_INFO
+} from '../../state/actions/asyncAPI';
+import { AsyncState } from '../../models/AsyncState';
+import { hideActivityModal, showActivityModal } from '../../state/actions/Modal';
 
 export interface BinningScreenProps {
   pallets: Pallet[] | [];
+  route: RouteProp<any, string>;
   dispatch: Dispatch<any>;
   navigation: NavigationProp<any>;
   isManualScanEnabled: boolean;
+  useEffectHook: (effect: EffectCallback, deps?: ReadonlyArray<any>) => void;
+  getPalletApi: AsyncState
 }
 export interface Pallet {
   palletId: number,
@@ -55,54 +71,148 @@ const ItemSeparator = () => <View style={styles.separator} />;
 
 export const BinningScreen = (props: BinningScreenProps): JSX.Element => {
   const {
-    pallets, isManualScanEnabled, dispatch, navigation
+    pallets, isManualScanEnabled, dispatch, navigation, route, useEffectHook, getPalletApi
   } = props;
 
+  let scannedSubscription: EmitterSubscription;
+
+  // Clear API state before leaving this screen
+  useEffectHook(() => navigation.addListener('beforeRemove', () => {
+    // TODO: we have to add warning Modal
+    dispatch({ type: GET_PALLET_INFO.RESET });
+  }), []);
+
+  // Scanner listener
+  useEffectHook(() => {
+    scannedSubscription = barcodeEmitter.addListener('scanned', scan => {
+      if (navigation.isFocused()) {
+        validateSession(navigation, route.name).then(() => {
+          trackEvent('pallet_scanned', {
+            barcode: scan.value,
+            type: scan.type
+          });
+          dispatch(getPalletInfo({ palletId: scan.value }));
+        });
+      }
+    });
+    return () => {
+      scannedSubscription.remove();
+    };
+  }, []);
+
+  useEffectHook(() => {
+    // on api success
+    if (!getPalletApi.isWaiting && getPalletApi.result) {
+      if (getPalletApi.result.status === 200) {
+        const {
+          data
+        } = getPalletApi.result;
+        const palletItem = pallets.filter(item => item.palletId === data.palletId);
+        if (palletItem.length > 0) {
+          Toast.show({
+            type: 'info',
+            text1: strings('PALLET.PALLET_EXISTS'),
+            visibilityTime: 3000,
+            position: 'bottom'
+          });
+        } else {
+          // TODO: dispatch action to store the pallet in redux after INTLSAOPS-5162 get completed
+          Toast.show({
+            type: 'success',
+            text1: strings('LOCATION.PALLET_ADDED'),
+            visibilityTime: 3000,
+            position: 'bottom'
+          });
+        }
+      } else if (getPalletApi.result.status === 204) {
+        Toast.show({
+          type: 'info',
+          text1: strings('LOCATION.PALLET_NOT_FOUND'),
+          visibilityTime: 3000,
+          position: 'bottom'
+        });
+      }
+      dispatch(hideActivityModal());
+    }
+    // on api error
+    if (!getPalletApi.isWaiting && getPalletApi.error) {
+      dispatch(hideActivityModal());
+      Toast.show({
+        type: 'error',
+        text1: strings('LOCATION.ADD_PALLET_ERROR'),
+        text2: strings('GENERICS.TRY_AGAIN'),
+        visibilityTime: 4000,
+        position: 'bottom'
+      });
+    }
+    // on api request
+    if (getPalletApi.isWaiting) {
+      dispatch(showActivityModal());
+    }
+  }, [getPalletApi]);
+
+  const handleUnhandledTouches = () => {
+    Keyboard.dismiss();
+    return false;
+  };
+
   return (
-    <View style={styles.container}>
-      {isManualScanEnabled && <ManualScan />}
-      <Text style={styles.helperText}>{strings('BINNING.SCAN_PALLET_BIN')}</Text>
-      <ItemSeparator />
-      <FlatList
-        data={pallets}
-        removeClippedSubviews={false}
-        ItemSeparatorComponent={ItemSeparator}
-        renderItem={binningItemCard}
-        keyExtractor={(item: any) => item.palletId.toString()}
-        ListEmptyComponent={(
-          <View style={styles.scanContainer}>
-            <TouchableOpacity onPress={() => openCamera()}>
-              <Icon size={100} name="barcode-scan" color={COLOR.BLACK} />
-            </TouchableOpacity>
-            <View style={styles.scanText}>
-              <Text>{strings('BINNING.SCAN_PALLET')}</Text>
+    <KeyboardAvoidingView
+      style={styles.safeAreaView}
+      behavior="height"
+      keyboardVerticalOffset={110}
+      onStartShouldSetResponder={handleUnhandledTouches}
+    >
+      <View style={styles.container}>
+        {isManualScanEnabled && <ManualScan />}
+        <Text style={styles.helperText}>{strings('BINNING.SCAN_PALLET_BIN')}</Text>
+        <ItemSeparator />
+        <FlatList
+          data={pallets}
+          removeClippedSubviews={false}
+          ItemSeparatorComponent={ItemSeparator}
+          renderItem={binningItemCard}
+          keyExtractor={(item: any) => item.palletId.toString()}
+          ListEmptyComponent={(
+            <View style={styles.scanContainer}>
+              <TouchableOpacity onPress={() => openCamera()}>
+                <Icon size={100} name="barcode-scan" color={COLOR.BLACK} />
+              </TouchableOpacity>
+              <View style={styles.scanText}>
+                <Text>{strings('BINNING.SCAN_PALLET')}</Text>
+              </View>
             </View>
-          </View>
         )}
-      />
-      <ItemSeparator />
-      <Button
-        title={strings('GENERICS.NEXT')}
-        type={Button.Type.PRIMARY}
-        style={styles.buttonWrapper}
-        disabled={pallets.length < 1}
-      />
-    </View>
+        />
+        <ItemSeparator />
+        <Button
+          title={strings('GENERICS.NEXT')}
+          type={Button.Type.PRIMARY}
+          style={styles.buttonWrapper}
+          disabled={pallets.length < 1}
+        />
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 const Binning = (): JSX.Element => {
   // TODO: pallets and binLocation needs to be connected to Redux
   const pallets: Pallet[] = [];
   const dispatch = useDispatch();
+  const route = useRoute();
   const navigation = useNavigation();
   const isManualScanEnabled = useTypedSelector(state => state.Global.isManualScanEnabled);
+  const getPalletApi = useTypedSelector(state => state.async.getPalletInfo);
 
   return (
     <BinningScreen
       pallets={pallets}
       dispatch={dispatch}
+      route={route}
       navigation={navigation}
+      useEffectHook={useEffect}
       isManualScanEnabled={isManualScanEnabled}
+      getPalletApi={getPalletApi}
     />
   );
 };
