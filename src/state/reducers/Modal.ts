e84e60646c978812b5087a25c19abbcd6cd15952
Nathan Edwards - n0e00s9 <Nathan.Edwards0@walmart.com>
2022-03-08 17:26:07 -0600

    Revert "update types for actions and reducers"

--- src/state/reducers/Modal.ts
+++ src/state/reducers/Modal.ts
@@ -1,23 +1,14 @@
 import {
-  Actions,
-  HIDE_ACTIVITY_MODAL,
-  HIDE_INFO_MODAL,
-  SHOW_ACTIVITY_MODAL,
-  SHOW_INFO_MODAL
+  HIDE_ACTIVITY_MODAL, HIDE_INFO_MODAL, SHOW_ACTIVITY_MODAL, SHOW_INFO_MODAL
 } from '../actions/Modal';
 
-interface ModalState {
-  showModal: boolean;
-  showActivity: boolean;
-  content: { title: string; text: string };
-}
-const initialState: ModalState = {
+const initialState = {
   showModal: false,
   showActivity: false,
-  content: { title: '', text: '' }
+  content: null
 };
 
-export const modal = (state = initialState, action: Actions): ModalState => {
+export const modal = (state = initialState, action: any) => {
   switch (action.type) {
     case SHOW_ACTIVITY_MODAL:
       return { ...state, showModal: true, showActivity: true };
@@ -25,10 +16,7 @@ export const modal = (state = initialState, action: Actions): ModalState => {
       return initialState;
     case SHOW_INFO_MODAL:
       return {
-        ...state,
-        showModal: true,
-        showActivity: false,
-        content: action.payload
+        ...state, showModal: true, showActivity: false, content: action.payload
       };
     case HIDE_INFO_MODAL:
       return initialState;
