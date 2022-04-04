68d66802989b155c9301abaa616679f76eb26255
Stefan Small <vn50pz2@homeoffice.wal-mart.com>
2022-04-04 13:02:24 -0400

    Properly append "Front" group to the top of the Flatlist

--- src/screens/SalesFloorTab/SalesFloorTabScreen.tsx
+++ src/screens/SalesFloorTab/SalesFloorTabScreen.tsx
@@ -1,36 +1,68 @@
-import { groupBy } from 'lodash';
 import React from 'react';
-import { FlatList, Text, View } from 'react-native';
+import { FlatList, View } from 'react-native';
 import ListGroup from '../../components/ListGroup/ListGroup';
 import { PickListItem } from '../../models/Picking';
 import { useNavigation, NavigationProp } from '@react-navigation/native';
+import { strings } from '../../locales';
 
 interface SalesFloorTabProps {
   picklist: PickListItem[];
   navigation: NavigationProp<any>;
 }
-const getZoneFromPalletLocation = (palletLocation: string | undefined) =>
-  palletLocation
-    ? palletLocation
-        .substring(0, palletLocation.indexOf('-'))
+
+const FRONT = 'Front';
+const getZoneFromPalletLocation = (pickItem: PickListItem) => {
+  return pickItem.palletLocationName && !pickItem.moveToFront
+    ? pickItem.palletLocationName
+        .substring(0, pickItem.palletLocationName.indexOf('-'))
         .replace(/[\d.]+$/, '')
-    : '';
+    : FRONT;
+};
 
 const SalesFloorTabScreen = (props: SalesFloorTabProps) => {
   const { picklist, navigation } = props;
-  const groupedPickListByZone = groupBy(picklist, (item: PickListItem) =>
-    getZoneFromPalletLocation(item.palletLocationName)
-  );
-  const pickListKeys = Object.keys(groupedPickListByZone);
+
+  const listGroupMap: Map<string, PickListItem[]> = new Map().set(FRONT, []);
+
+  // Assigns each picklist item to a zone or the moveToFront group
+  picklist.forEach(pickItem => {
+    let zoneName = getZoneFromPalletLocation(pickItem);
+    if (zoneName === FRONT) {
+      const movedToFrontList = listGroupMap.get(FRONT);
+      if (movedToFrontList) {
+        movedToFrontList?.push(pickItem);
+        listGroupMap.set(FRONT, movedToFrontList);
+      }
+    }
+    if (zoneName !== FRONT && listGroupMap.has(zoneName)) {
+      let zoneItems = listGroupMap.get(zoneName);
+      if (zoneItems) {
+        zoneItems.push(pickItem);
+        listGroupMap.set(zoneName, zoneItems);
+      }
+    } else if (zoneName !== FRONT && !listGroupMap.has(zoneName)) {
+      listGroupMap.set(zoneName, [pickItem]);
+    }
+  });
+
+  const picklistGroup = Object.fromEntries(listGroupMap);
+  // Sort all keys excluding Front group
+  const picklistZoneKeys = Object.keys(picklistGroup)
+    .filter(keyName => keyName !== FRONT)
+    .sort((a, b) => (a > b ? 1 : -1));
+  // Set Front group as the first index
+  const allPickKeys = [FRONT, ...picklistZoneKeys];
 
   return (
     <View>
       <FlatList
-        data={pickListKeys}
+        data={allPickKeys}
         renderItem={({ item }) => (
           <ListGroup
-            title={`${item} (${groupedPickListByZone[item].length})`}
-            pickListItems={groupedPickListByZone[item]}
+            title={`${item === FRONT ? strings('PICKING.FRONT') : item} (${
+              picklistGroup[item] ? picklistGroup[item].length : 0
+            })`}
+            pickListItems={picklistGroup[item]}
             groupItems={false}
             navigation={navigation}
           />
@@ -43,7 +75,8 @@ const SalesFloorTabScreen = (props: SalesFloorTabProps) => {
 const SalesFloorTab = (props: { picklist: PickListItem[] }) => {
   const navigation = useNavigation();
   return (
-  <SalesFloorTabScreen picklist={props.picklist} navigation={navigation}/>
-)};
+    <SalesFloorTabScreen picklist={props.picklist} navigation={navigation} />
+  );
+};
 
 export default SalesFloorTab;
