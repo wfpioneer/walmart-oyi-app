c68490bf1220ac2086a4af02124dd20522b17d0d
Kyle Welch <kyle.welch@walmart.com>
2022-04-07 08:03:38 -0500

    started adding button stuff for screen

--- src/screens/PickBinWorkflow/PickBinWorkflowScreen.tsx
+++ src/screens/PickBinWorkflow/PickBinWorkflowScreen.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useTypedSelector } from '../../state/reducers/RootReducer';
 
-const PickBinWorkflowScreen = () => (
-  <View>
-    <Text>PickBinWorkflow</Text>
-  </View>
-);
-const PickBinWorkflow = () => <PickBinWorkflowScreen />;
+interface PBWorkflowProps {
+  userFeatures: string[];
+}
+
+const PickBinWorkflowScreen = (props: PBWorkflowProps) => {
+  const { userFeatures } = props;
+
+  const actionButtonsView = () => {
+    const buttonList: JSX.Element[] = [];
+    return buttonList;
+  };
+
+  return (
+    <SafeAreaView>
+      <Text>PickBinWorkflow</Text>
+      <View>
+        {actionButtonsView()}
+      </View>
+    </SafeAreaView>
+  );
+};
+const PickBinWorkflow = () => {
+  const userFeatures = useTypedSelector(state => state.User.features);
+  return (
+    <PickBinWorkflowScreen
+      userFeatures={userFeatures}
+    />
+  );
+};
 
 export default PickBinWorkflow;
