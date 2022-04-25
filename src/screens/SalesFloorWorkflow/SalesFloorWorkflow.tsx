bc54d3e87dd15ad3426976dff2b8ba01376df1e9
Kyle Welch <kyle.welch@walmart.com>
2022-04-25 10:10:54 -0500

    did more additions and updated tests

--- src/screens/SalesFloorWorkflow/SalesFloorWorkflow.tsx
+++ src/screens/SalesFloorWorkflow/SalesFloorWorkflow.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  FlatList, SafeAreaView, Text, View
+  ActivityIndicator,
+  FlatList, SafeAreaView, Text, TouchableOpacity, View
 } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { Dispatch } from 'redux';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
+import Toast from 'react-native-toast-message';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Button from '../../components/buttons/Button';
 import PickPalletInfoCard from '../../components/PickPalletInfoCard/PickPalletInfoCard';
 import SalesFloorItemCard, { MAX } from '../../components/SalesFloorItemCard/SalesFloorItemCard';
@@ -14,25 +17,76 @@ import { updatePicks } from '../../state/actions/Picking';
 import { PickListItem } from '../../models/Picking.d';
 import { strings } from '../../locales';
 import styles from './SalesFloorWorkflow.style';
-import { Pallet, PalletInfo } from '../../models/PalletManagementTypes';
-import { UseEffectType } from '../../models/Generics.d';
+import { PalletItemDetails } from '../../models/PalletManagementTypes';
+import { UseEffectType, UseStateType } from '../../models/Generics.d';
 import { AsyncState } from '../../models/AsyncState';
 import { getPalletDetails } from '../../state/actions/saga';
+import COLOR from '../../themes/Color';
+import { GET_PALLET_DETAILS } from '../../state/actions/asyncAPI';
 
 interface SFWorklfowProps {
   pickingState: PickingState;
-  palletToWork: Pallet;
   dispatch: Dispatch<any>;
   navigation: NavigationProp<any>;
   useEffectHook: UseEffectType;
   palletDetailsApi: AsyncState;
+  expirationState: UseStateType<string>;
+  perishableItemsState: UseStateType<Array<number>>;
+  perishableCategories: number[];
+  backupCategories: string;
 }
+
+export const palletDetailsApiEffect = (
+  navigation: NavigationProp<any>,
+  palletDetailsApi: AsyncState,
+  selectedPicks: PickListItem[],
+  dispatch: Dispatch<any>,
+  setExpiration: UseStateType<string>[1],
+  setPerishableItems: UseStateType<Array<number>>[1],
+  perishableCategories: number[]
+) => {
+  if (navigation.isFocused() && !palletDetailsApi.isWaiting) {
+    // success
+    if (palletDetailsApi.result) {
+      const { pallets }: { pallets: PalletItemDetails[] } = palletDetailsApi.result.data;
+      const pallet = pallets[0];
+
+      const quantifiedPicks: PickListItem[] = [];
+
+      selectedPicks.forEach(pick => {
+        const itemWithQty = pallet.items.find(palletItem => pick.itemNbr === palletItem.itemNbr);
+        const initialQty = itemWithQty?.quantity || 0;
+        quantifiedPicks.push({ ...pick, quantityLeft: initialQty });
+      });
+
+      const palletPerishableItems = pallet.items.reduce((perishableItems: number[], palletItem) => (perishableCategories
+        .includes(palletItem.categoryNbr || 0)
+        ? [...perishableItems, Number(palletItem.itemNbr)]
+        : perishableItems),
+      []);
+
+      setPerishableItems(palletPerishableItems);
+      setExpiration(pallet.expirationDate || '');
+      dispatch(updatePicks(quantifiedPicks));
+      dispatch({ type: GET_PALLET_DETAILS.RESET });
+    }
+  }
+};
 
 export const SalesFloorWorkflowScreen = (props: SFWorklfowProps) => {
   const {
-    pickingState, palletToWork, palletDetailsApi,
-    dispatch, navigation, useEffectHook
+    pickingState, palletDetailsApi,
+    dispatch, navigation, useEffectHook,
+    expirationState, perishableItemsState,
+    perishableCategories, backupCategories
   } = props;
+
+  const perishableCats = perishableCategories.length
+    ? perishableCategories
+    : backupCategories.split(',').map(Number);
+
+  const [expirationDate, setExpiration] = expirationState;
+  const [perishableItems, setPerishableItems] = perishableItemsState;
 
   const selectedPicks = pickingState.pickList.filter(pick => pickingState.selectedPicks.includes(pick.id));
   const assigned = selectedPicks[0].assignedAssociate;
@@ -45,18 +99,15 @@ export const SalesFloorWorkflowScreen = (props: SFWorklfowProps) => {
     return navListener;
   });
 
-  useEffectHook(() => {
-    if (navigation.isFocused() && !palletDetailsApi.isWaiting) {
-      // success
-      if (palletDetailsApi.result) {
-        const { pallets }: { pallets: Pallet[] } = palletDetailsApi.result.data;
-        const pallet = pallets[0];
-        pallet.items.forEach(item => {
-          selectedPicks.findIndex
-        })
-      }
-    }
-  }, [palletDetailsApi]);
+  useEffectHook(() => palletDetailsApiEffect(
+    navigation,
+    palletDetailsApi,
+    selectedPicks,
+    dispatch,
+    setExpiration,
+    setPerishableItems,
+    perishableCats
+  ), [palletDetailsApi]);
 
   const handleComplete = () => {};
 
@@ -83,6 +134,33 @@ export const SalesFloorWorkflowScreen = (props: SFWorklfowProps) => {
       dispatch(updatePicks([{ ...item, quantityLeft: newQuantity }]));
     }
   };
+
+  if (palletDetailsApi.isWaiting) {
+    return (
+      <ActivityIndicator
+        animating={palletDetailsApi.isWaiting}
+        hidesWhenStopped
+        color={COLOR.MAIN_THEME_COLOR}
+        size="large"
+        style={styles.activityIndicator}
+      />
+    );
+  }
+
+  if (palletDetailsApi.error) {
+    return (
+      <View style={styles.errorView}>
+        <MaterialIcons name="error" size={60} color={COLOR.RED_300} />
+        <Text style={styles.errorText}>{strings('PALLET.PALLET_DETAILS_ERROR')}</Text>
+        <TouchableOpacity
+          style={styles.errorButton}
+          onPress={() => dispatch(getPalletDetails({ palletIds: [selectedPicks[0].palletId] }))}
+        >
+          <Text>{strings('GENERICS.RETRY')}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 
   const renderItem = ({ item }: { item: PickListItem }) => (
     <SalesFloorItemCard
@@ -141,26 +219,24 @@ export const SalesFloorWorkflowScreen = (props: SFWorklfowProps) => {
 const SalesFloorWorkflow = () => {
   const pickingState = useTypedSelector(state => state.Picking);
   const palletDetailsApi = useTypedSelector(state => state.async.getPalletDetails);
+  const { perishableCategories } = useTypedSelector(state => state.PalletManagement);
+  const { backupCategories } = useTypedSelector(state => state.User.configs);
   const dispatch = useDispatch();
   const navigation = useNavigation();
-
-  const mockPallet: Pallet = {
-    items: [],
-    palletInfo: {
-      id: 1,
-      createDate: 'gestern',
-      expirationDate: 'morgen'
-    }
-  };
+  const expirationState = useState('');
+  const perishableItemsState = useState<Array<number>>([]);
 
   return (
     <SalesFloorWorkflowScreen
       pickingState={pickingState}
-      palletToWork={mockPallet}
       dispatch={dispatch}
       navigation={navigation}
       useEffectHook={useEffect}
       palletDetailsApi={palletDetailsApi}
+      expirationState={expirationState}
+      perishableItemsState={perishableItemsState}
+      perishableCategories={perishableCategories}
+      backupCategories={backupCategories}
     />
   );
 };
