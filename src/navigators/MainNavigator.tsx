ae640ff0326b963141a84d5b2c22065520beb41f
Stefan Small <vn50pz2@homeoffice.wal-mart.com>
2022-04-29 16:12:21 -0400

    disable scanner if app is running in the background

--- src/navigators/MainNavigator.tsx
+++ src/navigators/MainNavigator.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { AppState, AppStateStatus } from 'react-native';
 import Login from '../screens/Login/Login';
 import LocationManagementNavigator from './LocationManagementNavigator';
 import TabNavigator from './TabNavigator';
@@ -10,24 +11,63 @@ import PalletManagementNavigator from './PalletManagementNavigator';
 import SettingsToolNavigator from './SettingsToolNavigator';
 import BinningNavigator from './BinningNavigator';
 import PickingNavigator from './PickingNavigator';
+import { disableScanner, enableScanner } from '../utils/scannerUtils';
 
 const Stack = createStackNavigator();
 
-export const MainNavigator = (): JSX.Element => (
-  <NavigationContainer>
-    <Stack.Navigator screenOptions={{
-      headerShown: false
-    }}
-    >
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Tabs" component={TabNavigator} />
-      <Stack.Screen name="ReviewItemDetails" component={ReviewItemDetailsNavigator} />
-      <Stack.Screen name="PrintPriceSign" component={PrintPriceSignNavigator} />
-      <Stack.Screen name="LocationManagement" component={LocationManagementNavigator} />
-      <Stack.Screen name="PalletManagement" component={PalletManagementNavigator} />
-      <Stack.Screen name="SettingsTool" component={SettingsToolNavigator} />
-      <Stack.Screen name="Binning" component={BinningNavigator} />
-      <Stack.Screen name="Picking" component={PickingNavigator} />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+export const MainNavigator = (): JSX.Element => {
+  const appState = useRef(AppState.currentState);
+  useEffect(() => {
+    const callAppState = (nextAppState: AppStateStatus) => {
+      if (
+        appState.current.match(/inactive|background/)
+        && nextAppState === 'active'
+      ) {
+        console.log('App has come to the foreground!');
+        enableScanner();
+      } else {
+        disableScanner();
+      }
+
+      appState.current = nextAppState;
+    };
+
+    AppState.addEventListener('change', callAppState);
+
+    return () => {
+      AppState.removeEventListener('change', callAppState);
+    };
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Tabs" component={TabNavigator} />
+        <Stack.Screen
+          name="ReviewItemDetails"
+          component={ReviewItemDetailsNavigator}
+        />
+        <Stack.Screen
+          name="PrintPriceSign"
+          component={PrintPriceSignNavigator}
+        />
+        <Stack.Screen
+          name="LocationManagement"
+          component={LocationManagementNavigator}
+        />
+        <Stack.Screen
+          name="PalletManagement"
+          component={PalletManagementNavigator}
+        />
+        <Stack.Screen name="SettingsTool" component={SettingsToolNavigator} />
+        <Stack.Screen name="Binning" component={BinningNavigator} />
+        <Stack.Screen name="Picking" component={PickingNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
