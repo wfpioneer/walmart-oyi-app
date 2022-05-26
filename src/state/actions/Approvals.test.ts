655847dfaf239ca30595c241a4dd64b97589514a
Ravivarman Thayumanavan <Ravi.Varman.Thayumanavan@walmart.com>
2022-05-26 22:48:31 +0530

    Addressed PR comments

--- src/state/actions/Approvals.test.ts
+++ src/state/actions/Approvals.test.ts
@@ -12,9 +12,8 @@ import {
 } from './Approvals';
 import { mockApprovals } from '../../mockData/mockApprovalList';
 
-describe('test action creators for Worklist', () => {
-  it('test action creators for Worklist', () => {
-    // toggleItem action
+describe('Manager Approvals action reducer', () => {
+  it('handles toggling-ON action for a item in Approvals list', () => {
     const testItemNbr = 250589;
     const testIsSelected = true;
     const testToggleItemPayload = {
@@ -26,18 +25,47 @@ describe('test action creators for Worklist', () => {
       type: TOGGLE_ITEM,
       payload: testToggleItemPayload
     });
-    // toggleCategory action
+  });
+  it('handles toggling-OFF action for a item in Approvals list', () => {
+    const testItemNbr = 250589;
+    const testIsSelected = false;
+    const testToggleItemPayload = {
+      itemNbr: testItemNbr,
+      isSelected: testIsSelected
+    };
+    const toggleItemResult = toggleItem(testItemNbr, testIsSelected);
+    expect(toggleItemResult).toStrictEqual({
+      type: TOGGLE_ITEM,
+      payload: testToggleItemPayload
+    });
+  });
+  it('handles toggling-ON action for a category of Approvals', () => {
     const testCategoryNbr = 2;
+    const testIsSelected = true;
     const testToggleCategoryPayload = {
       categoryNbr: 2,
-      isSelected: false
+      isSelected: testIsSelected
     };
-    const toggleCategoriesResult = toggleCategory(testCategoryNbr, false);
+    const toggleCategoriesResult = toggleCategory(testCategoryNbr, testIsSelected);
     expect(toggleCategoriesResult).toStrictEqual({
       type: TOGGLE_CATEGORY,
       payload: testToggleCategoryPayload
     });
-    // toggleAllItems action
+  });
+  it('handles toggling-OFF action for a category of Approvals', () => {
+    const testCategoryNbr = 2;
+    const testIsSelected = false;
+    const testToggleCategoryPayload = {
+      categoryNbr: 2,
+      isSelected: testIsSelected
+    };
+    const toggleCategoriesResult = toggleCategory(testCategoryNbr, testIsSelected);
+    expect(toggleCategoriesResult).toStrictEqual({
+      type: TOGGLE_CATEGORY,
+      payload: testToggleCategoryPayload
+    });
+  });
+  it('handles toggling-ON action for all items in Approvals list', () => {
     const testSelectAll = true;
     const toggleAllItemsResult = toggleAllItems(testSelectAll);
     expect(toggleAllItemsResult).toStrictEqual({
@@ -46,7 +74,18 @@ describe('test action creators for Worklist', () => {
         selectAll: testSelectAll
       }
     });
-    // setApprovalList action
+  });
+  it('handles toggling-OFF action for all items in Approvals list', () => {
+    const testSelectAll = false;
+    const toggleAllItemsResult = toggleAllItems(testSelectAll);
+    expect(toggleAllItemsResult).toStrictEqual({
+      type: TOGGLE_ALL_ITEMS,
+      payload: {
+        selectAll: testSelectAll
+      }
+    });
+  });
+  it('handles setApprovalList action for showing the Approvals list in the Approval screen', () => {
     const setApprovalListResult = setApprovalList(mockApprovals, [0]);
     expect(setApprovalListResult).toStrictEqual({
       type: SET_APPROVAL_LIST,
@@ -55,7 +94,8 @@ describe('test action creators for Worklist', () => {
         headerIndices: [0]
       }
     });
-    // resetApprovals action
+  });
+  it('handles resetApprovals action to reset the Approval list to its initial state', () => {
     const resetApprovalsResult = resetApprovals();
     expect(resetApprovalsResult).toStrictEqual({
       type: RESET_APPROVALS
--- src/state/reducers/Approvals.test.ts
+++ src/state/reducers/Approvals.test.ts
@@ -9,63 +9,105 @@ import { ApprovalState, Approvals, initialState } from './Approvals';
 import { mockApprovals } from '../../mockData/mockApprovalList';
 import { convertApprovalListData } from '../../screens/ApprovalList/ApprovalList';
 
-describe('testing Worklist reducer', () => {
-  it('testing Worklist reducer', () => {
-    // Intitial State
-    const testInitialState = initialState;
-    const { filteredData, headerIndices } = convertApprovalListData(mockApprovals);
-    // Changed state
-    const testChangedState: ApprovalState = {
-      ...initialState,
-      categories: {
-        1: {
-          checkedItemQty: 0,
-          totalItemQty: 1
-        },
-        2: {
-          checkedItemQty: 0,
-          totalItemQty: 3
-        }
+describe('The Manager Approvals Reducer', () => {
+  // Intitial State
+  const testInitialState = initialState;
+  const { filteredData, headerIndices } = convertApprovalListData(mockApprovals);
+  // Changed state
+  const testChangedState: ApprovalState = {
+    ...initialState,
+    categories: {
+      1: {
+        checkedItemQty: 0,
+        totalItemQty: 1
       },
-      approvalList: filteredData,
-      categoryIndices: headerIndices
-    };
-    let testResults = Approvals(testInitialState, setApprovalList(filteredData, headerIndices));
+      2: {
+        checkedItemQty: 0,
+        totalItemQty: 3
+      }
+    },
+    approvalList: filteredData,
+    categoryIndices: headerIndices
+  };
+  it('handles setting the Approval list for the Manager', () => {
+    const testResults = Approvals(testInitialState, setApprovalList(filteredData, headerIndices));
     expect(testResults).toStrictEqual(testChangedState);
-    testResults = Approvals(testChangedState, toggleCategory(1, true));
+  });
+  it('handles toggling-ON a category of approvals', () => {
+    const testResults = Approvals(testChangedState, toggleCategory(1, true));
+    const mockIsSelected = true;
     testChangedState.selectedItemQty = 1;
     testChangedState.categories[1].checkedItemQty = 1;
-    testChangedState.approvalList[0].isChecked = true;
-    testChangedState.approvalList[1].isChecked = true;
+    testChangedState.approvalList[0].isChecked = mockIsSelected;
+    testChangedState.approvalList[1].isChecked = mockIsSelected;
     expect(testResults).toStrictEqual(testChangedState);
-    testResults = Approvals(testChangedState, toggleCategory(1, false));
+  });
+  it('handles toggling-OFF a category of approvals', () => {
+    const mockIsSelected = false;
+    const testResults = Approvals(testChangedState, toggleCategory(1, false));
     testChangedState.selectedItemQty = 0;
     testChangedState.categories[1].checkedItemQty = 0;
-    testChangedState.approvalList[0].isChecked = false;
-    testChangedState.approvalList[1].isChecked = false;
+    testChangedState.approvalList[0].isChecked = mockIsSelected;
+    testChangedState.approvalList[1].isChecked = mockIsSelected;
     expect(testResults).toStrictEqual(testChangedState);
+  });
+  it('hndles toggling-ON a item available in Approvals list', () => {
     const mockItemNumber = 123;
     const mockIsSelected = true;
-    testResults = Approvals(testChangedState, toggleItem(mockItemNumber, mockIsSelected));
+    const testResults = Approvals(testChangedState, toggleItem(mockItemNumber, mockIsSelected));
     testChangedState.selectedItemQty = 1;
     testChangedState.categories[1].checkedItemQty = 1;
     testChangedState.approvalList[0].isChecked = true;
     testChangedState.approvalList[1].isChecked = true;
     expect(testResults).toStrictEqual(testChangedState);
+  });
+  it('handles toggling-OFF a item available in Approvals list', () => {
+    const mockItemNumber = 123;
+    const mockIsSelected = false;
+    const testResults = Approvals(testChangedState, toggleItem(mockItemNumber, mockIsSelected));
+    testChangedState.selectedItemQty = 0;
+    testChangedState.categories[1].checkedItemQty = 0;
+    testChangedState.approvalList[0].isChecked = mockIsSelected;
+    testChangedState.approvalList[1].isChecked = mockIsSelected;
+    expect(testResults).toStrictEqual(testChangedState);
+  });
+  it('handles Maintaining the previously selected items State on page refresh', () => {
+    // setting the state with few items as selected for Approval
+    testChangedState.selectedItemQty = 1;
+    testChangedState.categories[1].checkedItemQty = 1;
+    testChangedState.approvalList[0].isChecked = true;
+    testChangedState.approvalList[1].isChecked = true;
+    const testResults = Approvals(testChangedState, setApprovalList(filteredData, headerIndices));
+    expect(testResults).toStrictEqual(testChangedState);
+  });
+  it('handles toggling-ON all items available in the Approvals list', () => {
     const mockIsSelectedAllItems = true;
-    testResults = Approvals(testChangedState, toggleAllItems(mockIsSelectedAllItems));
-    testChangedState.isAllSelected = true;
+    const testResults = Approvals(testChangedState, toggleAllItems(mockIsSelectedAllItems));
+    testChangedState.isAllSelected = mockIsSelectedAllItems;
     testChangedState.selectedItemQty = 4;
     testChangedState.categories[1].checkedItemQty = testChangedState.categories[1].totalItemQty;
     testChangedState.categories[2].checkedItemQty = testChangedState.categories[2].totalItemQty;
     testChangedState.approvalList = testChangedState.approvalList.map(list => {
-      list.isChecked = true;
+      list.isChecked = mockIsSelectedAllItems;
       return list;
     });
     expect(testResults).toStrictEqual(testChangedState);
-    testResults = Approvals(testChangedState, setApprovalList(filteredData, headerIndices));
+  });
+  it('handles toggling-OFF all items available in the Approvals list', () => {
+    const mockIsSelectedAllItems = false;
+    const testResults = Approvals(testChangedState, toggleAllItems(mockIsSelectedAllItems));
+    testChangedState.isAllSelected = mockIsSelectedAllItems;
+    testChangedState.selectedItemQty = 0;
+    testChangedState.categories[1].checkedItemQty = 0;
+    testChangedState.categories[2].checkedItemQty = 0;
+    testChangedState.approvalList = testChangedState.approvalList.map(list => {
+      list.isChecked = mockIsSelectedAllItems;
+      return list;
+    });
     expect(testResults).toStrictEqual(testChangedState);
-    testResults = Approvals(testChangedState, resetApprovals());
+  });
+  it('handle reset Approvals to bring the Approvals state to initial State', () => {
+    const testResults = Approvals(testChangedState, resetApprovals());
     expect(testResults).toStrictEqual(testInitialState);
   });
 });
