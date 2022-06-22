460cecad6804f0288289aab39bad2ec616cdf77b
Ravivarman Thayumanavan <Ravi.Varman.Thayumanavan@walmart.com>
2022-06-22 17:58:30 +0530

    Reverted changes that added for testing functionality

--- src/navigators/BinningNavigator.tsx
+++ src/navigators/BinningNavigator.tsx
@@ -4,7 +4,6 @@ import { useDispatch } from 'react-redux';
 import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { createStackNavigator } from '@react-navigation/stack';
 import Binning from '../screens/Binning/Binning';
-import ScanPallet from '../screens/ScanPallet/ScanPallet';
 import AssignLocation from '../screens/AssignLocation/AssignLocation';
 import COLOR from '../themes/Color';
 import { strings } from '../locales';
@@ -66,9 +65,9 @@ export const BinningNavigatorStack = (props: BinningNavigatorProps): JSX.Element
     >
       <Stack.Screen
         name="Binning"
-        component={ScanPallet}
+        component={Binning}
         options={{
-          headerTitle: strings('WORKLIST.SCAN_PALLET'),
+          headerTitle: strings('BINNING.BINNING'),
           headerRight: () => (
             <View style={styles.headerContainer}>
               {renderScanButton(dispatch, isManualScanEnabled)}
