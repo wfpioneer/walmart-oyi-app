701dbd20b3efdc683cef65af66d1b5bec119c151
Ravivarman Thayumanavan <Ravi.Varman.Thayumanavan@walmart.com>
2022-07-06 16:31:33 +0530

    Implemented worklist header for Missing Pallet Worklist

---
+++ src/screens/Worklist/WorklistHeader/WorklistHeader.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from './WorklistHeader.style';
+import { strings } from '../../../locales';
+
+interface worklistHeaderPropsI {
+  title: string;
+  numberOfItems: number;
+}
+
+const WorklistHeader = (props: worklistHeaderPropsI) => (
+  <View style={styles.container}>
+    <Text style={styles.headerTitle}>
+      { props.title }
+    </Text>
+    <Text style={styles.numberOfItems}>
+      { `${props.numberOfItems} ${props.numberOfItems === 1 ? strings('GENERICS.ITEM') : strings('GENERICS.ITEMS')}`}
+    </Text>
+  </View>
+);
+
+export default WorklistHeader;
