ba4b81bd6c3f81599506f7556acede02034ef9a6
Stefan Small <vn50pz2@homeoffice.wal-mart.com>
2022-07-27 14:45:43 -0400

    update tests

--- src/screens/AdditionalItemHistory/AdditionalItemHistory.tsx
+++ src/screens/AdditionalItemHistory/AdditionalItemHistory.tsx
@@ -7,33 +7,34 @@ import { mockOHChangeHistory } from '../../mockData/getItemDetails';
 import styles from './AdditionalItemHistory.style';
 
 interface AdditionalHistoryProps {
-  data: OHChangeHistory[];
+  onHandsData: OHChangeHistory[];
 }
 export const AdditionalItemHistoryScreen = (props: AdditionalHistoryProps) => {
-  const { data } = props;
+  const { onHandsData } = props;
 
-  const historyData = [...data].sort((a, b) => {
+  const historyData = [...onHandsData].sort((a, b) => {
     const date1 = new Date(a.initiatedTimestamp);
     const date2 = new Date(b.initiatedTimestamp);
     return date2 > date1 ? 1 : -1;
   });
   return (
-    <View>
+    <View style={styles.container}>
       <FlatList
-        data={historyData}
+        data={historyData.slice(0, 30)}
         renderItem={({ item }) => (
           <View style={styles.historyCard}>
             <Text>{moment(item.initiatedTimestamp).format('YYYY-MM-DD')}</Text>
             <Text>{item.oldQuantity}</Text>
           </View>
         )}
+        keyExtractor={item => item.id.toString()}
       />
     </View>
   );
 };
 
 const AdditionalItemHistory = () => (
-  <AdditionalItemHistoryScreen data={mockOHChangeHistory} />
+  <AdditionalItemHistoryScreen onHandsData={mockOHChangeHistory} />
 );
 
 export default AdditionalItemHistory;
