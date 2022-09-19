f55cec4e2bcc91aba9f4c07864b47789c2c24b67
Ravivarman Thayumanavan <Ravi.Varman.Thayumanavan@walmart.com>
2022-09-19 21:44:07 +0530

    Implemented Location List Card UI Component

---
+++ src/components/LocationListCard/LocationListCard.test.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react-native';
+import LocationListCard, { LocationList } from './LocationListCard';
+
+jest.mock('react-native-vector-icons/MaterialCommunityIcons', () => 'Icon');
+
+const mockLocationList: LocationList[] = [{
+  sectionId: 1502,
+  locationName: 'A1-1',
+  quantity: '22',
+  palletId: '4928',
+  increment: jest.fn,
+  decrement: jest.fn,
+  delete: jest.fn
+}];
+
+describe('Tests LocationListCard Component', () => {
+  it('should tests redering LocationListCard with location list', async () => {
+    const { toJSON } = render(
+      <LocationListCard
+        locationList={mockLocationList}
+        locationType="floor"
+        loading={false}
+        error={false}
+        onRetry={jest.fn}
+        scanRequired={false}
+      />
+    );
+    expect(toJSON()).toMatchSnapshot();
+  });
+  it('should tests rendering LocationListCard with error prop as true', async () => {
+    const { toJSON } = render(
+      <LocationListCard
+        locationList={[]}
+        locationType="floor"
+        loading={false}
+        error={true}
+        onRetry={jest.fn}
+        scanRequired={false}
+      />
+    );
+    expect(toJSON()).toMatchSnapshot();
+  });
+  it('should tests rendering LocationListCard with loader while fetching async API data', async () => {
+    const { toJSON } = render(
+      <LocationListCard
+        locationList={[]}
+        locationType="floor"
+        loading={true}
+        error={false}
+        onRetry={jest.fn}
+        scanRequired={false}
+      />
+    );
+    expect(toJSON()).toMatchSnapshot();
+  });
+  it('Tests add location button press functionality', () => {
+    const mockAddCallbackFn = jest.fn();
+    const { getByTestId } = render(
+      <LocationListCard
+        locationList={mockLocationList}
+        locationType="floor"
+        loading={false}
+        add={mockAddCallbackFn}
+        error={false}
+        onRetry={jest.fn}
+        scanRequired={false}
+      />
+    );
+    const addLocationButton = getByTestId('add-location');
+    fireEvent.press(addLocationButton);
+    expect(mockAddCallbackFn).toBeCalledTimes(1);
+  });
+  it('Tests retry button press functionality while there is an error', () => {
+    const mockRetryCallbackFn = jest.fn();
+    const { getByTestId } = render(
+      <LocationListCard
+        locationList={[]}
+        locationType="floor"
+        loading={false}
+        add={jest.fn}
+        error={true}
+        onRetry={mockRetryCallbackFn}
+        scanRequired={false}
+      />
+    );
+    const retryButton = getByTestId('retry-button');
+    fireEvent.press(retryButton);
+    expect(mockRetryCallbackFn).toBeCalledTimes(1);
+  });
+});
