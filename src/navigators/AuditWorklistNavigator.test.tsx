a454ff1c4c33b4966be0d6a5b82668729c6ebbcc
Stefan Small <vn50pz2@homeoffice.wal-mart.com>
2022-09-21 17:11:17 -0400

    Add filtermenu to audit wl screen

--- src/navigators/AuditWorklistNavigator.test.tsx
+++ src/navigators/AuditWorklistNavigator.test.tsx
@@ -16,6 +16,7 @@ describe('AuditItemWorklist Navigator', () => {
         isManualScanEnabled={false}
         auditWorklists={false}
         navigation={navigationProp}
+        menuOpen={false}
       />
     );
     expect(renderer.getRenderOutput()).toMatchSnapshot();
--- src/navigators/AuditWorklistNavigator.tsx
+++ src/navigators/AuditWorklistNavigator.tsx
@@ -2,18 +2,29 @@ import { HeaderBackButton } from '@react-navigation/elements';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React, { Dispatch } from 'react';
-import { TouchableOpacity, View } from 'react-native';
+import { Animated, TouchableOpacity, View } from 'react-native';
+import SideMenu from 'react-native-side-menu-updated';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { useDispatch } from 'react-redux';
 import { strings } from '../locales';
 import AuditWorklist from '../screens/Worklist/AuditWorklist/AuditWorklistScreen';
+import { FilterMenu } from '../screens/Worklist/FilterMenu/FilterMenu';
 import { setManualScan } from '../state/actions/Global';
+import { toggleMenu } from '../state/actions/Worklist';
 import { useTypedSelector } from '../state/reducers/RootReducer';
 import COLOR from '../themes/Color';
 import styles from './AuditWorklistNavigator.style';
 import AuditWorklistTabs from './AuditWorklistTabNavigator/AuditWorklistTabNavigator';
 
 const Stack = createStackNavigator();
+
+interface AuditWorklistNavProps {
+  auditWorklists: boolean;
+  dispatch: Dispatch<any>;
+  isManualScanEnabled: boolean;
+  navigation: NavigationProp<any>;
+  menuOpen: boolean;
+}
 
 export const renderScanButton = (
   dispatch: Dispatch<any>,
@@ -43,26 +54,27 @@ const renderPrintButton = () => (
   </TouchableOpacity>
 );
 
-const renderFilterButton = () => (
-  <TouchableOpacity onPress={() => {}}>
+const onFilterMenuPress = (dispatch: Dispatch<any>, menuOpen: boolean) => {
+  if (menuOpen) {
+    dispatch(toggleMenu(false));
+  } else {
+    dispatch(toggleMenu(true));
+  }
+};
+
+const renderFilterButton = (dispatch: Dispatch<any>, menuOpen: boolean) => (
+  <TouchableOpacity onPress={() => onFilterMenuPress(dispatch, menuOpen)}>
     <View style={styles.filterButton}>
       <MaterialCommunityIcons name="filter-variant" size={30} color={COLOR.WHITE} />
     </View>
   </TouchableOpacity>
 );
 
-interface AuditWorklistNavProps {
-  auditWorklists: boolean;
-  dispatch: Dispatch<any>;
-  isManualScanEnabled: boolean;
-  navigation: NavigationProp<any>;
-}
-
 export const AuditWorklistNavigatorStack = (
   props: AuditWorklistNavProps
 ): JSX.Element => {
   const {
-    auditWorklists, dispatch, isManualScanEnabled, navigation
+    auditWorklists, dispatch, isManualScanEnabled, navigation, menuOpen
   } = props;
 
   const navigateBack = () => {
@@ -73,46 +85,64 @@ export const AuditWorklistNavigatorStack = (
     }
   };
   return (
-    <Stack.Navigator
-      screenOptions={() => ({
-        headerMode: 'float',
-        headerStyle: { backgroundColor: COLOR.MAIN_THEME_COLOR },
-        headerTintColor: COLOR.WHITE
+    <SideMenu
+      menu={<FilterMenu />}
+      menuPosition="right"
+      isOpen={menuOpen}
+      animationFunction={(prop, value) => Animated.spring(prop, {
+        toValue: value,
+        friction: 8,
+        useNativeDriver: true
+      })}
+      onChange={(isOpen => {
+        if (!isOpen && menuOpen) {
+          dispatch(toggleMenu(false));
+        } else if (isOpen && !menuOpen) {
+          dispatch(toggleMenu(true));
+        }
       })}
     >
-      <Stack.Screen
-        name="AuditWorklistTabs"
-        component={AuditWorklistTabs}
-        options={{
-          headerTitle: strings('WORKLIST.AUDIT_WORKLIST'),
-          headerLeft: hlProps => hlProps.canGoBack && (
-          <HeaderBackButton
-                // eslint-disable-next-line react/jsx-props-no-spreading
-            {...hlProps}
-            onPress={navigateBack}
-          />
-          ),
-          headerRight: () => (
-            <View style={styles.headerContainer}>
-              {renderFilterButton()}
-            </View>
-          )
-        }}
-      />
-      <Stack.Screen
-        name="AuditWorklist"
-        component={AuditWorklist}
-        options={{
-          headerTitle: strings('AUDITS.AUDIT_ITEM'),
-          headerRight: () => (
-            <View style={styles.headerContainer}>
-              {renderPrintButton()}
-              {renderScanButton(dispatch, isManualScanEnabled)}
-            </View>
-          )
-        }}
-      />
-    </Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={() => ({
+          headerMode: 'float',
+          headerStyle: { backgroundColor: COLOR.MAIN_THEME_COLOR },
+          headerTintColor: COLOR.WHITE
+        })}
+      >
+        <Stack.Screen
+          name="AuditWorklistTabs"
+          component={AuditWorklistTabs}
+          options={{
+            headerTitle: strings('WORKLIST.AUDIT_WORKLIST'),
+            headerLeft: hlProps => hlProps.canGoBack && (
+            <HeaderBackButton
+            // eslint-disable-next-line react/jsx-props-no-spreading
+              {...hlProps}
+              onPress={navigateBack}
+            />
+            ),
+            headerRight: () => (
+              <View style={styles.headerContainer}>
+                {renderFilterButton(dispatch, menuOpen)}
+              </View>
+            )
+          }}
+        />
+        <Stack.Screen
+          name="AuditWorklist"
+          component={AuditWorklist}
+          options={{
+            headerTitle: strings('AUDITS.AUDIT_ITEM'),
+            headerRight: () => (
+              <View style={styles.headerContainer}>
+                {renderPrintButton()}
+                {renderScanButton(dispatch, isManualScanEnabled)}
+              </View>
+            )
+          }}
+        />
+      </Stack.Navigator>
+    </SideMenu>
   );
 };
 
@@ -121,12 +151,15 @@ const AuditWorklistNavigator = (): JSX.Element => {
   const dispatch = useDispatch();
   const { isManualScanEnabled } = useTypedSelector(state => state.Global);
   const { auditWorklists } = useTypedSelector(state => state.User.configs);
+  const { menuOpen } = useTypedSelector(state => state.Worklist);
+
   return (
     <AuditWorklistNavigatorStack
       auditWorklists={auditWorklists}
       dispatch={dispatch}
       navigation={navigation}
       isManualScanEnabled={isManualScanEnabled}
+      menuOpen={menuOpen}
     />
   );
 };
