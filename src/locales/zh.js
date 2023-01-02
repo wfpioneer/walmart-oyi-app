21bf2a8f53e7075463c8f45e6253af2a99595adb
Stefan Small <vn50pz2@homeoffice.wal-mart.com>
2023-01-02 09:15:27 -0500

    update translation

--- src/locales/zh.js
+++ src/locales/zh.js
@@ -144,7 +144,7 @@ export default {
     GROSS_PROFIT: '毛利',
     HISTORY: '历史信息',
     NO_HISTORY: '无历史记录信息',
-    RESERVE_ADJUSTMENT: 'Reserve Adjustment' // TODO translations needed
+    RESERVE_ADJUSTMENT: '储位库存数量调整'
   },
   PRINT: {
     MAIN_TITLE: '打印价签',
