1d3f26f8601abc4a4c416a63054ab2eb6c55dc07
pandurang <chandrakant.argade.p.chandrakant.argade@walmart.com>
2023-01-27 18:54:32 +0530

    unit test update

--- src/components/LocationItemCard/LocationItemCard.test.tsx
+++ src/components/LocationItemCard/LocationItemCard.test.tsx
@@ -1,8 +1,58 @@
 import React from 'react';
+import { fireEvent, render } from '@testing-library/react-native';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import { NavigationProp } from '@react-navigation/native';
 import LocationItemCard from './LocationItemCard';
 import { LocationType } from '../../models/LocationType';
+
+jest.mock('../../utils/AppCenterTool.ts', () => ({
+  ...jest.requireActual('../../utils/__mocks__/AppCenterTool'),
+  trackEvent: jest.fn()
+}));
+
+jest.mock('react-native-vector-icons/MaterialCommunityIcons', () => 'mockMaterialCommunityIcons');
+
+// const mockDispatch = jest.fn();
+// jest.mock('react-redux', () => {
+//   const ActualReactRedux = jest.requireActual('react-redux');
+//   return {
+//     ...ActualReactRedux,
+//     useDispatch: () => mockDispatch
+//   };
+// });
+
+const navigationProp: NavigationProp<any> = {
+  addListener: jest.fn(),
+  canGoBack: jest.fn(),
+  dispatch: jest.fn(),
+  goBack: jest.fn(),
+  isFocused: jest.fn(() => true),
+  removeListener: jest.fn(),
+  reset: jest.fn(),
+  setOptions: jest.fn(),
+  setParams: jest.fn(),
+  navigate: jest.fn(),
+  getState: jest.fn(),
+  getParent: jest.fn(),
+  getId: jest.fn()
+};
+
+jest.mock('@react-navigation/native', () => {
+  const actualNav = jest.requireActual('@react-navigation/native');
+  return {
+    ...actualNav,
+    useNavigation: () => ({
+      navigate: jest.fn(),
+      dispatch: jest.fn(),
+      isFocused: jest.fn().mockReturnValue(true),
+      goBack: jest.fn()
+    }),
+    useRoute: () => ({
+      key: 'test',
+      name: 'test'
+    })
+  };
+});
 
 const mockZoneItem = {
   zoneId: 1,
@@ -23,12 +73,12 @@ const mockSectionItem = {
   palletCount: 4
 };
 
-let navigationProp: NavigationProp<any>;
-
 describe('Test Location Item Card', () => {
-  it('Renders Location Item Card with mock Zone data', () => {
-    const renderer = ShallowRenderer.createRenderer();
-    renderer.render(
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  it('Renders Location Item Card with mock Zone data with card button disabled', () => {
+    const { toJSON } = render(
       <LocationItemCard
         location={mockZoneItem.zoneName}
         locationId={mockZoneItem.zoneId}
@@ -38,52 +88,111 @@ describe('Test Location Item Card', () => {
         locationDetails={`${mockZoneItem.aisleCount} Aisles`}
         navigator={navigationProp}
         destinationScreen={LocationType.AISLE}
-        locationPopupVisible={false}
+        locationPopupVisible={true}
         trackEventCall={jest.fn()}
       />
     );
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('Renders Location Item Card with mock Zone data with card click', () => {
+    const mockDispatch = jest.fn();
+    const mockTrackEvent = jest.fn();
+    const { getByTestId, toJSON } = render(
+      <LocationItemCard
+        location={mockZoneItem.zoneName}
+        locationId={mockZoneItem.zoneId}
+        locationType={LocationType.ZONE}
+        locationName={mockZoneItem.zoneName}
+        dispatch={mockDispatch}
+        locationDetails={`${mockZoneItem.aisleCount} Aisles`}
+        navigator={navigationProp}
+        destinationScreen={LocationType.AISLE}
+        locationPopupVisible={false}
+        trackEventCall={mockTrackEvent}
+      />
+    );
+
+    const btnCard = getByTestId('btnCard');
+    fireEvent.press(btnCard);
+    expect(mockDispatch).toBeCalledTimes(1);
+    expect(mockDispatch).toBeCalledWith({ payload: { id: 1, name: 'G - Grocery' }, type: 'LOCATION/SELECT_ZONE' });
+    expect(mockTrackEvent).toBeCalledWith(
+      'Zone_List', { action: 'location_item_card_clicked', destinationScreen: 'Aisles', locationName: 'G - Grocery' }
+    );
+    expect(navigationProp.navigate).toHaveBeenCalledWith('Aisles');
+    expect(toJSON()).toMatchSnapshot();
   });
 });
 
 describe('Test Location Item Card', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   it('Renders Location Item Card with mock Aisle data', () => {
-    const renderer = ShallowRenderer.createRenderer();
-    renderer.render(
+    const mockDispatch = jest.fn();
+    const mockTrackEvent = jest.fn();
+    const { getByTestId, toJSON } = render(
       <LocationItemCard
         location={mockAisleItem.aisleName}
         locationId={mockAisleItem.aisleId}
         locationType={LocationType.AISLE}
         locationName={mockAisleItem.aisleName}
-        dispatch={jest.fn()}
+        dispatch={mockDispatch}
         locationDetails={`${mockAisleItem.sectionCount} Sections`}
         navigator={navigationProp}
         destinationScreen={LocationType.SECTION}
         locationPopupVisible={false}
-        trackEventCall={jest.fn()}
+        trackEventCall={mockTrackEvent}
       />
     );
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+
+    const btnCard = getByTestId('btnCard');
+    fireEvent.press(btnCard);
+    expect(mockDispatch).toBeCalledTimes(1);
+    expect(mockDispatch).toBeCalledWith({ payload: { id: 1, name: 'Aisle G1' }, type: 'LOCATION/SELECT_AISLE' });
+    expect(mockTrackEvent).toBeCalledWith(
+      'Aisle_List', { action: 'location_item_card_clicked', destinationScreen: 'Sections', locationName: 'Aisle G1' }
+    );
+    expect(navigationProp.navigate).toHaveBeenCalledWith('Sections');
+    expect(toJSON()).toMatchSnapshot();
   });
 });
 
 describe('Test Location Item Card', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   it('Renders Location Item Card with mock Section data', () => {
-    const renderer = ShallowRenderer.createRenderer();
-    renderer.render(
+    const mockDispatch = jest.fn();
+    const mockTrackEvent = jest.fn();
+    const { getByTestId, toJSON } = render(
       <LocationItemCard
         location={mockSectionItem.sectionName}
         locationId={mockSectionItem.sectionId}
         locationType={LocationType.SECTION}
         locationName={mockSectionItem.sectionName}
-        dispatch={jest.fn()}
+        dispatch={mockDispatch}
         locationDetails={`${mockSectionItem.itemCount} Items, ${mockSectionItem.palletCount} Pallets`}
         navigator={navigationProp}
         destinationScreen={LocationType.SECTION_DETAILS}
         locationPopupVisible={false}
-        trackEventCall={jest.fn()}
+        trackEventCall={mockTrackEvent}
       />
     );
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+
+    const btnCard = getByTestId('btnCard');
+    fireEvent.press(btnCard);
+    expect(mockDispatch).toBeCalledTimes(2);
+    expect(mockDispatch).toBeCalledWith(
+      { payload: { id: 1, name: 'Section G1-1' }, type: 'LOCATION/SELECT_SECTION' }
+    );
+    expect(mockDispatch).toBeCalledWith({ type: 'GLOBAL/RESET_SCANNED_EVENT' });
+    expect(mockTrackEvent).toBeCalledWith(
+      'Section_List',
+      { action: 'location_item_card_clicked', destinationScreen: 'SectionDetails', locationName: 'Section G1-1' }
+    );
+    expect(navigationProp.navigate).toHaveBeenCalledWith('SectionDetails');
+    expect(toJSON()).toMatchSnapshot();
   });
 });
