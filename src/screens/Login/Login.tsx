9d99dcfa1ae2b1aebbd8dacb29524026dd160617
t0s00og <Timothy.Strickland@walmart.com>
2023-06-28 09:31:47 -0500

    updating userId

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -49,7 +49,7 @@ import {
 
 export const resetClubConfigApiState = () => ({ type: GET_CLUB_CONFIG.RESET });
 export const resetFluffyFeaturesApiState = () => ({ type: GET_FLUFFY_ROLES.RESET });
-const DOMAIN = 'wm-BusinessUnitCategory';
+const DOMAIN = 'wm-BusinessUnitType';
 const CLUB_NBR = 'wm-BusinessUnitNumber';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
