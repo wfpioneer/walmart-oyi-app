8d680a47c76fa4766654a4c6d64b8f0a4d54c359
Stefan Small <vn50pz2@homeoffice.wal-mart.com>
2023-08-08 15:05:20 -0400

    add test case for SaveAuditsProgress

---
+++ src/services/SaveAuditsProgress.service.test.ts
@@ -0,0 +1,25 @@
+import { AxiosResponse } from 'axios';
+import { getEnvironment } from '../utils/environment';
+import Request, { mockAxiosResponse } from './Request';
+import SaveAuditsProgressService from './SaveAuditsProgress.service';
+
+describe('SaveAuditProgress Service Tests', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('calls getAuditLocations', async () => {
+    const getAudLoc = SaveAuditsProgressService.getAuditLocations;
+    mockAxiosResponse(undefined);
+    const mockResponse = { data: {}, status: 200, statusText: 'ok' } as AxiosResponse;
+    const getRequestSpy = jest.spyOn(Request, 'get');
+    getRequestSpy.mockResolvedValue(mockResponse);
+    const urls = getEnvironment();
+
+    const getAudLocResponse = await getAudLoc({ itemNbr: 123, hours: 5 });
+    expect(getRequestSpy).toHaveBeenCalledWith(
+      `${urls.orchestrationURL}/worklist/audits/${123}?timelimit=5`
+    );
+    expect(getAudLocResponse).toStrictEqual(mockResponse);
+  });
+});
