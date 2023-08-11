85d2d6689c8ba33a2cb030a38d2ab26d7e9b742e
Kyle Welch <k0w0dpx@homeoffice.wal-mart.com>
2023-08-11 18:15:13 -0500

    rearranged service tes lines

--- src/services/GetWorklist.service.test.ts
+++ src/services/GetWorklist.service.test.ts
@@ -10,24 +10,24 @@ describe('Get Worklist Service Tests', () => {
 
   // TODO refactor test Spike story for Mock Service Worker
   it('ensures that the get audit worklist v1 function calls correctly', async () => {
-    const urls = getEnvironment();
-    const baseUrl = `${urls.worklistURL}/worklist/v1/audits`;
     const getAuditWorklistV1 = GetWorklistService.getWorklistAuditV1;
     mockAxiosResponse(undefined);
     const mockResponse = { data: [], status: 200, statusText: 'ok' } as AxiosResponse;
-    const getSpy = jest.spyOn(Request, 'get');
-    getSpy.mockResolvedValue(mockResponse);
+    const getRequestSpy = jest.spyOn(Request, 'get');
+    getRequestSpy.mockResolvedValue(mockResponse);
+    const urls = getEnvironment();
+    const baseUrl = `${urls.worklistURL}/worklist/v1/audits`;
 
     let getWorklistResponse = await getAuditWorklistV1({ worklistType: ['AU', 'RA'] });
     expect(getWorklistResponse).toStrictEqual(mockResponse);
-    expect(getSpy).toHaveBeenCalledWith(`${baseUrl}?type=AU&type=RA`);
-    getSpy.mockClear();
+    expect(getRequestSpy).toHaveBeenCalledWith(`${baseUrl}?type=AU&type=RA`);
+    getRequestSpy.mockClear();
 
     getWorklistResponse = await getAuditWorklistV1({ worklistType: [] });
-    expect(getSpy).toHaveBeenCalledWith(baseUrl);
-    getSpy.mockClear();
+    expect(getRequestSpy).toHaveBeenCalledWith(baseUrl);
+    getRequestSpy.mockClear();
 
     getWorklistResponse = await getAuditWorklistV1({});
-    expect(getSpy).toHaveBeenCalledWith(baseUrl);
+    expect(getRequestSpy).toHaveBeenCalledWith(baseUrl);
   });
 });
