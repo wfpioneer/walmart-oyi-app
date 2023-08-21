4fc0f644230351cbc27930f8e6a3d8d02c7c6dd0
Kyle Welch <k0w0dpx@homeoffice.wal-mart.com>
2023-08-21 13:19:36 -0500

    removed react import from test file

--- src/components/UnsavedWarningModal/UnsavedWarningModal.test.tsx
+++ src/components/UnsavedWarningModal/UnsavedWarningModal.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render } from '@testing-library/react-native';
-import React from 'react';
 import { renderUnsavedWarningModal } from './UnsavedWarningModal';
 
 const mockSetState = jest.fn();
