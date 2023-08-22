a1446d11eeea806b683cbc1a0cd99c3a5118c65a
t0s00og <Timothy.Strickland@walmart.com>
2023-08-22 12:11:09 -0500

    implementing cn images

---
+++ src/services/ItemCenterToken.service.test.ts
@@ -0,0 +1,18 @@
+import { AxiosResponse } from 'axios';
+import { getEnvironment } from '../utils/environment';
+import Request, { mockAxiosResponse } from './Request';
+import ItmCenterTokenService from './ItemCenterToken.service';
+
+describe('SaveAuditProgress Service Tests', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  // TODO refactor test Spike story for Mock Service Worker
+  it('ensures that the put - save function calls correctly', () => {
+    const { getItemCenterToken } = ItmCenterTokenService;
+    const postRequestSpy = jest.spyOn(Request, 'post');
+    getItemCenterToken();
+    expect(postRequestSpy).toHaveBeenCalled();
+  });
+});
