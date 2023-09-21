6ed96933f20611795cf33f9e23d37a0a4b2d65e4
Stefan Small <vn50pz2@homeoffice.wal-mart.com>
2023-09-21 13:28:51 -0400

    move deletePalletUpc service into deletePallet service

--- src/services/DeletePallet.service.ts
+++ src/services/DeletePallet.service.ts
@@ -18,4 +18,28 @@ export default class DeletePalletService {
       `${urls.orchestrationURL}/pallet/${payload.palletId}`
     );
   }
+
+  // Removes a pallet from an item or multiple items
+  public static deletePalletUPCs(payload: {
+    palletId: string;
+    upcs: string[],
+    expirationDate?: string,
+    removeExpirationDate: boolean
+  }) {
+    const upcsUrlParam = payload.upcs.reduce((reducer, current, index, array) => {
+      if (index !== array.length - 1) {
+        // TODO: eslint issue is not easily resolvable and we many need tech debt story to fix this issue.
+        return reducer.concat(`upcs=${current}&`);
+      }
+      return reducer.concat(`upcs=${current}`);
+    }, '');
+    const urls: Environment = getEnvironment();
+    return Request.delete(
+      `${urls.locationUrl}/pallet/${payload.palletId}/upc?${upcsUrlParam}`,
+      {
+        removeExpirationDate: payload.removeExpirationDate,
+        expirationDate: payload.expirationDate
+      }
+    );
+  }
 }
